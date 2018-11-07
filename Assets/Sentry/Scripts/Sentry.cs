725a24f30186ee409a6c684663278243d50875a8
Bruno Garcia <bruno@brunogarcia.com>
2018-11-07 09:20:05 +0100

    release 0.0.2

--- Assets/Sentry/Scripts/Sentry.cs
+++ Assets/Sentry/Scripts/Sentry.cs
@@ -265,7 +265,7 @@ namespace Sentry
     public class SdkVersion
     {
         public string name = "sentry.unity.lite";
-        public string version = "0.0.2";
+        public string version = "0.0.3";
     }
 
     [Serializable]
