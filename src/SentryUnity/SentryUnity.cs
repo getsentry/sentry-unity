7556196c2f94d1619032e885a415d8cb0d0ae35e
Bruno Garcia <bruno@brunogarcia.com>
2020-10-03 13:53:58 -0400

    hi Unity

---
+++ src/SentryUnity/SentryUnity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using UnityEngine;
+
+namespace SentryUnity
+{
+    public class SentryUnity
+    {
+        public static void Init()
+        {
+            Debug.LogWarning("Sentry ini");
+        }
+    }
+}
