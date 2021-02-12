0d9dfa75621b00736a8ff8fcbfdbd92fb5424806
semuserable <semuserable@gmail.com>
2021-02-12 02:03:42 +0200

    package-dev structure
    Co-authored-by: Bruno Garcia <bruno@brunogarcia.com>

---
+++ package-dev/README.md
@@ -0,0 +1,5 @@
+Separate `UPM` which is used for developemnt.
+* `package` - release `UPM`
+* `package-dev` - dev `UPM`
+
+Build artifacts from `src` folder put into `package-dev`.
---
+++ src/Sentry.Unity.Tests/SimpleTests.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+using UnityEngine;
+
+namespace Sentry.Unity.Tests
+{
+    public sealed class SimpleTests
+    {
+
+        [Test]
+        public void SentryUnity_Object_NotNull()
+        {
+            var unitySentryOptions = ScriptableObject.CreateInstance<UnitySentryOptions>();
+            Assert.NotNull(unitySentryOptions);
+        }
+    }
+}
