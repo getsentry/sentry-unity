9ec381e491c574d215ba73bd0ded03547325e8b4
semuserable <semuserable@gmail.com>
2021-02-12 11:47:36 +0200

    Libs for coroutine support in Unity tests; coroutine example test added

--- src/Sentry.Unity.Tests/SimpleTests.cs
+++ src/Sentry.Unity.Tests/SimpleTests.cs
@@ -1,16 +1,26 @@
-﻿using NUnit.Framework;
+﻿using System.Collections;
+using NUnit.Framework;
 using UnityEngine;
+using UnityEngine.TestTools;
 
 namespace Sentry.Unity.Tests
 {
     public sealed class SimpleTests
     {
-
         [Test]
         public void SentryUnity_Object_NotNull()
         {
             var unitySentryOptions = ScriptableObject.CreateInstance<UnitySentryOptions>();
             Assert.NotNull(unitySentryOptions);
+        }
+
+        [UnityTest]
+        public IEnumerator UnityCoroutine_CreatedGameObjectWithDelay_Found()
+        {
+            var cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            cube.name = "test cube";
+            yield return new WaitForSeconds(2);
+            Assert.IsTrue(GameObject.Find(cube.name));
         }
     }
 }
