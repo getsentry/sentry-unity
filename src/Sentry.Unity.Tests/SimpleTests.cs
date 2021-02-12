9af30b8684fafe5be271df794da7ff5c8d2307ba
Bruno Garcia <bruno@brunogarcia.com>
2021-02-11 19:03:32 -0500

    Update src/Sentry.Unity.Tests/SimpleTests.cs

--- src/Sentry.Unity.Tests/SimpleTests.cs
+++ src/Sentry.Unity.Tests/SimpleTests.cs
@@ -5,9 +5,6 @@ namespace Sentry.Unity.Tests
 {
     public sealed class SimpleTests
     {
-        [Test]
-        public void Simple_TwoPlusTwo_EqualsFour()
-            => Assert.AreEqual(4, 2 + 2);
 
         [Test]
         public void SentryUnity_Object_NotNull()
