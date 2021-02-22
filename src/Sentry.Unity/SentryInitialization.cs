ac8fd1688ec0799c1fdd77be0698abb54a88ff6a
semuserable <semuserable@gmail.com>
2021-02-22 20:05:08 +0200

    Integration tests example, Sentry infra aligned to support that (#58)

--- src/Sentry.Unity/SentryInitialization.cs
+++ src/Sentry.Unity/SentryInitialization.cs
@@ -1,4 +1,3 @@
-using Sentry.Protocol;
 using System;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,11 +6,27 @@ using CompressionLevel = System.IO.Compression.CompressionLevel;
 
 namespace Sentry.Unity
 {
+    public interface IEventCapture
+    {
+        SentryId Capture(SentryEvent sentryEvent);
+    }
+
+    internal class EventCapture : IEventCapture
+    {
+        public SentryId Capture(SentryEvent sentryEvent)
+            => SentrySdk.CaptureEvent(sentryEvent);
+    }
+
     // https://94677106febe46b88b9b9ae5efd18a00@o447951.ingest.sentry.io/5439417
     public static class SentryInitialization
     {
         private static long _timeLastError;
-        public static long MinTime { get; } = TimeSpan.FromMilliseconds(500).Ticks;
+
+        // TODO: Temp solution until a proper event bandwidth throttling is implemented
+        public static long MinTimeTicks = TimeSpan.FromMilliseconds(500).Ticks;
+
+        // Temp event capture infra
+        public static IEventCapture EventCapture = new EventCapture();
 
         [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
         public static void Init()
@@ -126,9 +141,12 @@ namespace Sentry.Unity
 
         private static void OnLogMessageReceived(string condition, string stackTrace, LogType type, UnitySentryOptions options)
         {
+            // TODO: 'options' not used yet
+            _ = options;
+
             var time = DateTime.UtcNow.Ticks;
 
-            if (time - _timeLastError <= MinTime)
+            if (time - _timeLastError <= MinTimeTicks)
             {
                 return;
             }
@@ -143,9 +161,9 @@ namespace Sentry.Unity
                 return;
             }
 
-            var evt = new SentryEvent(new UnityLogException(condition, stackTrace));
-            evt.SetTag("log.type", ToEventTagType(type));
-            _ = SentrySdk.CaptureEvent(evt);
+            var sentryEvent = new SentryEvent(new UnityLogException(condition, stackTrace));
+            sentryEvent.SetTag("log.type", ToEventTagType(type));
+            _ = EventCapture?.Capture(sentryEvent);
             SentrySdk.AddBreadcrumb(condition, level: ToBreadcrumbLevel(type));
         }
 
