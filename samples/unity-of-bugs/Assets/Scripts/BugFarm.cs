f1023d216325736327980ed4d644dec70d7ea8cf
Tyrrrz <tyrrrz@gmail.com>
2021-03-02 00:16:07 +0200

    wip

--- samples/unity-of-bugs/Assets/Scripts/BugFarm.cs
+++ samples/unity-of-bugs/Assets/Scripts/BugFarm.cs
@@ -102,7 +102,8 @@ public class BugFarm : MonoBehaviour
 
     public void CrashNative()
     {
-        crash();
+        // Doesn't work on CI
+        //crash();
     }
 
     [MethodImpl(MethodImplOptions.NoInlining)]
