a53badbb6b1529ac0ce734d6d3f6b02fb174812b
semuserable <semuserable@gmail.com>
2021-03-24 20:21:32 +0200

    clean up

--- src/Sentry.Unity/SentryInitialization.cs
+++ src/Sentry.Unity/SentryInitialization.cs
@@ -1,7 +1,5 @@
 using System;
 using System.Collections.Generic;
-using System.IO;
-using System.Text.Json;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using CompressionLevel = System.IO.Compression.CompressionLevel;
