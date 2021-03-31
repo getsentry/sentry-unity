05b0b1a67fef9e85d2011511ce57ad3239ea0f4f
Bruno Garcia <bruno@brunogarcia.com>
2021-03-30 23:13:46 -0400

    fix bump script

--- scripts/bump-version.sh
+++ scripts/bump-version.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 set -eux
-# craft executes this file by convension.
 
 # Requires powershell: `brew install powershell`
-pwsh ./scripts/bump-version.ps1 $1
+# craft executes this file by convension, passing the new version as the second argument:
+pwsh ./scripts/bump-version.ps1 $2
