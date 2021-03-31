993dd2c8040fc75e1e07a3716679bde52bf1cca1
Bruno Garcia <bruno@brunogarcia.com>
2021-03-30 22:48:06 -0400

    craft (#87)

---
+++ scripts/bump-version.ps1
@@ -0,0 +1,13 @@
+param([string] $newVersion)
+
+$utf8NoBomEncoding = New-Object System.Text.UTF8Encoding($False) 
+function Replace-TextInFile {
+    param([string] $filePath, [string] $pattern, [string] $replacement)
+
+    $content = [IO.File]::ReadAllText($filePath)
+    $content = [Text.RegularExpressions.Regex]::Replace($content, $pattern, $replacement)
+    [IO.File]::WriteAllText($filePath, $content, $utf8NoBomEncoding)
+}
+
+Replace-TextInFile "$PSScriptRoot/../src/Directory.Build.props" '(?<=<Version>)(.*?)(?=</Version>)' $newVersion
+Replace-TextInFile "$PSScriptRoot/../package/package.json" '(?<="version": ")(.*?)(?=")' $newVersion
