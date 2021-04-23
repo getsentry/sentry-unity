daa3157b42e6a02b2f07ba80ae55143a179862a8
semuserable <semuserable@gmail.com>
2021-04-23 19:25:12 +0300

    Minor refactor

---
+++ src/Sentry.Unity/EventCapture.cs
@@ -0,0 +1,13 @@
+﻿namespace Sentry.Unity
+{
+    internal interface IEventCapture
+    {
+        SentryId Capture(SentryEvent sentryEvent);
+    }
+
+    internal class EventCapture : IEventCapture
+    {
+        public SentryId Capture(SentryEvent sentryEvent)
+            => SentrySdk.CaptureEvent(sentryEvent);
+    }
+}
