6ea8ad452e6676d96dfb2ec93cdb3eb765d448d6
Bruno Garcia <bruno@brunogarcia.com>
2021-05-09 23:51:51 -0400

    wip

--- src/build-android.sh
+++ src/build-android.sh
@@ -1,3 +1,7 @@
 pushd sentry-java
-./gradlew :sentry-android-core:assembleRelease :sentry-android-ndk:assembleRelease 
-cp 
+# :sentry:jar ?
+./gradlew :sentry-android-core:assembleRelease :sentry-android-ndk:assembleRelease :sentry:jar
+cp sentry-android-ndk/build/outputs/aar/sentry-android-ndk-release.aar ../../package-dev/Plugins/Android 
+cp sentry-android-core/build/outputs/aar/sentry-android-core-release.aar ../../package-dev/Plugins/Android 
+# building snapshot based on version, i.e: sentry-5.0.0-beta.3-SNAPSHOT.jar
+cp sentry/build/libs/sentry*.jar ../../package-dev/Plugins/Android 
