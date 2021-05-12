496ae76ee4ff27b995ca5ad2fc95cc1ec774b03d
Bruno Garcia <bruno@brunogarcia.com>
2021-05-11 21:22:29 -0400

    fix/compression level auto (#160)

--- src/Sentry.Unity/SentryUnity.cs
+++ src/Sentry.Unity/SentryUnity.cs
@@ -3,17 +3,28 @@ using System.ComponentModel;
 
 namespace Sentry.Unity
 {
+    /// <summary>
+    /// Sentry Unity initialization class.
+    /// </summary>
     public static class SentryUnity
     {
-        [EditorBrowsable(EditorBrowsableState.Never)]
-        public static void Init(SentryUnityOptions sentryUnityOptions)
-            => SentrySdk.Init(sentryUnityOptions);
-
+        /// <summary>
+        /// Initializes Sentry Unity SDK while configuring the options.
+        /// </summary>
+        /// <param name="unitySentryOptionsConfigure">Callback to configure the options.</param>
         public static void Init(Action<SentryUnityOptions> unitySentryOptionsConfigure)
         {
             var unitySentryOptions = new SentryUnityOptions();
             unitySentryOptionsConfigure.Invoke(unitySentryOptions);
             Init(unitySentryOptions);
         }
+
+        /// <summary>
+        /// Initializes Sentry Unity SDK while providing an options object.
+        /// </summary>
+        /// <param name="unitySentryOptions">The options object.</param>
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        public static void Init(SentryUnityOptions unitySentryOptions)
+            => SentrySdk.Init(unitySentryOptions);
     }
 }
