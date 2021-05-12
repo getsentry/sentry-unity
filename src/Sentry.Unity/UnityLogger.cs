58a010249feab23fe05e92341756f8757f29cfd6
semuserable <semuserable@gmail.com>
2021-05-12 14:28:37 +0300

    Sentry UnityLogger alligned to Unity Debug api

--- src/Sentry.Unity/UnityLogger.cs
+++ src/Sentry.Unity/UnityLogger.cs
@@ -4,19 +4,54 @@ using System;
 using UnityEngine;
 using static System.String;
 
+internal interface IUnityLoggerInterceptor
+{
+    void Intercept(string logMessage);
+}
+
 internal class UnityLogger : IDiagnosticLogger
 {
     private readonly SentryLevel _minimalLevel;
+    private readonly IUnityLoggerInterceptor? _interceptor;
+
     public bool IsEnabled(SentryLevel level) => level >= _minimalLevel;
-    public UnityLogger(SentryLevel minimalLevel) => _minimalLevel = minimalLevel;
+
+    public UnityLogger(SentryLevel minimalLevel, IUnityLoggerInterceptor? interceptor = null)
+    {
+        _minimalLevel = minimalLevel;
+        _interceptor = interceptor;
+    }
 
     public void Log(SentryLevel logLevel, string? message, Exception? exception = null, params object?[] args)
     {
-        if (IsEnabled(logLevel))
+        if (!IsEnabled(logLevel))
         {
-            Debug.Log($@"Sentry: {logLevel}
+            return;
+        }
+
+        switch (logLevel)
+        {
+            case SentryLevel.Debug or SentryLevel.Info:
+                Debug.Log(GetLog());
+                break;
+            case SentryLevel.Warning:
+                Debug.LogWarning(GetLog());
+                break;
+            case SentryLevel.Error or SentryLevel.Fatal:
+                Debug.LogError(GetLog());
+                break;
+            default:
+                Debug.Log(GetLog());
+                break;
+        }
+
+        string GetLog()
+        {
+            var log = $@"Sentry: {logLevel}
 {Format(message, args)}
-{exception}");
+{exception}";
+            _interceptor?.Intercept(log);
+            return log;
         }
     }
 
---
+++ test/Sentry.Unity.Tests/UnityLoggerTests.cs
@@ -0,0 +1,54 @@
+﻿using NUnit.Framework;
+using UnityEngine.TestTools;
+
+namespace Sentry.Unity.Tests
+{
+    public sealed class UnityLoggerTests
+    {
+        [Test]
+        public void Log_DebugLevels_Correspond([Values] SentryLevel sentryLevel)
+        {
+            LogAssert.ignoreFailingMessages = true;
+
+            var interceptor = new TestUnityLoggerInterceptor();
+            var logger = new UnityLogger(sentryLevel, interceptor);
+
+            const string expectedLog = "Some log";
+            logger.Log(sentryLevel, expectedLog);
+
+            Assert.True(logger.IsEnabled(sentryLevel));
+            Assert.True(interceptor.LogMessage.Contains("Sentry"));
+            Assert.True(interceptor.LogMessage.Contains(expectedLog));
+            Assert.True(interceptor.LogMessage.Contains(sentryLevel.ToString()));
+        }
+
+        [TestCaseSource(nameof(SentryLevels))]
+        public void Log_LowerLevelThanInitializationLevel_DisablesLogger(SentryLevel initializationLevel, SentryLevel lowerLevel)
+        {
+            var interceptor = new TestUnityLoggerInterceptor();
+            var logger = new UnityLogger(initializationLevel, interceptor);
+
+            const string expectedLog = "Some log";
+
+            logger.Log(lowerLevel, expectedLog);
+
+            Assert.False(logger.IsEnabled(lowerLevel));
+            Assert.False(interceptor.LogMessage.Contains(expectedLog));
+        }
+
+        private static object[] SentryLevels =
+        {
+            new object[] { SentryLevel.Info, SentryLevel.Debug },
+            new object[] { SentryLevel.Warning, SentryLevel.Info },
+            new object[] { SentryLevel.Error, SentryLevel.Warning },
+            new object[] { SentryLevel.Fatal, SentryLevel.Error }
+        };
+
+        private sealed class TestUnityLoggerInterceptor : IUnityLoggerInterceptor
+        {
+            public string LogMessage { get; private set; } = string.Empty;
+
+            public void Intercept(string logMessage) => LogMessage = logMessage;
+        }
+    }
+}
