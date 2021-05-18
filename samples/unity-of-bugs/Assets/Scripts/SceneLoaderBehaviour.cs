c88b81a8ea1a28f310467605bc3942308e2078eb
semuserable <semuserable@gmail.com>
2021-05-18 09:12:48 +0300

    SceneLoaderBehaviour

---
+++ samples/unity-of-bugs/Assets/Scripts/SceneLoaderBehaviour.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Assets.Scripts
+{
+    public sealed class SceneLoaderBehaviour : MonoBehaviour
+    {
+        public void LoadTransitionScene() => SceneManager.LoadScene("TransitionScene");
+
+        public void LoadBugFarmScene() => SceneManager.LoadScene("BugFarmScene");
+    }
+}
