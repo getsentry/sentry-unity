ae1f106e1a5a523b24b13ce3f7ed7fab07ed370e
semuserable <semuserable@gmail.com>
2021-05-18 18:39:51 +0300

    BugFarm in a namespace and sealed; TransitionScene transform reset

--- samples/unity-of-bugs/Assets/Scripts/BugFarm.cs
+++ samples/unity-of-bugs/Assets/Scripts/BugFarm.cs
@@ -6,121 +6,124 @@ using System.Runtime.CompilerServices;
 using UnityEngine;
 using UnityEngine.Assertions;
 
-public class BugFarm : MonoBehaviour
+namespace Assets.Scripts
 {
-    public const bool IsIL2CPP
-#if ENABLE_IL2CPP
-        = true;
-#else
-        = false;
-#endif
-
-    private void Start()
+    public sealed class BugFarm : MonoBehaviour
     {
-        Debug.Log("Sample Start 🦋");
-        Debug.LogWarning("Here come the bugs 🐞🦋🐛🐜🕷!");
-    }
+        public const bool IsIL2CPP
+    #if ENABLE_IL2CPP
+            = true;
+    #else
+            = false;
+    #endif
 
-    void Update()
-    {
-    }
-
-    public void AssertFalse() => Assert.AreEqual(true, false);
-
-    [MethodImpl(MethodImplOptions.NoInlining)]
-    public void ThrowNull()
-    {
-        throw null;
-    }
-
-    public void ThrowExceptionAndCatch()
-    {
-        Debug.Log("Throwing an instance of 🐛 CustomException!");
-
-        try
+        private void Start()
         {
-            throw new CustomException("Custom bugs 🐛🐛🐛🐛.");
+            Debug.Log("Sample Start 🦋");
+            Debug.LogWarning("Here come the bugs 🐞🦋🐛🐜🕷!");
         }
-        catch (Exception e)
+
+        void Update()
         {
-            Debug.LogException(e);
         }
-    }
 
-    public void ThrowNullAndCatch()
-    {
-        Debug.Log("Throwing 'null' and catching 🐜🐜🐜 it!");
+        public void AssertFalse() => Assert.AreEqual(true, false);
 
-        try
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        public void ThrowNull()
         {
-            ThrowNull();
+            throw null;
         }
-        catch (Exception e)
+
+        public void ThrowExceptionAndCatch()
         {
-            Debug.LogException(e);
+            Debug.Log("Throwing an instance of 🐛 CustomException!");
+
+            try
+            {
+                throw new CustomException("Custom bugs 🐛🐛🐛🐛.");
+            }
+            catch (Exception e)
+            {
+                Debug.LogException(e);
+            }
         }
-    }
 
-    public void SendMessage()
-    {
-        Debug.Log("🕷️🕷️🕷️ Spider message 🕷️🕷️🕷️🕷️");
-    }
-
-    public void ExceptionToString()
-    {
-        Debug.Log("Throw/Catch, Debug.LogError: Exception.ToString!");
-
-        try
+        public void ThrowNullAndCatch()
         {
-            MethodB();
+            Debug.Log("Throwing 'null' and catching 🐜🐜🐜 it!");
+
+            try
+            {
+                ThrowNull();
+            }
+            catch (Exception e)
+            {
+                Debug.LogException(e);
+            }
         }
-        catch (Exception e)
+
+        public void SendMessage()
         {
-            Debug.LogError($"ExceptionToString:\n{e}");
+            Debug.Log("🕷️🕷️🕷️ Spider message 🕷️🕷️🕷️🕷️");
         }
-    }
 
-    public void ThrowKotlin()
-    {
-#if UNITY_ANDROID
-        var jo = new AndroidJavaObject("unity.of.bugs.KotlinPlugin");
-        jo.CallStatic("throw");
-#else
-        Debug.LogWarning("Not on Android.");
-#endif
-    }
+        public void ExceptionToString()
+        {
+            Debug.Log("Throw/Catch, Debug.LogError: Exception.ToString!");
 
-    public void ThrowKotlinOnBackground()
-    {
-#if UNITY_ANDROID
-        var jo = new AndroidJavaObject("unity.of.bugs.KotlinPlugin");
-        jo.CallStatic("throwOnBackgroundThread");
-#else
-        Debug.LogWarning("Not on Android.");
-#endif
-    }
+            try
+            {
+                MethodB();
+            }
+            catch (Exception e)
+            {
+                Debug.LogError($"ExceptionToString:\n{e}");
+            }
+        }
 
-    public void CrashNative()
-    {
+        public void ThrowKotlin()
+        {
+    #if UNITY_ANDROID
+            var jo = new AndroidJavaObject("unity.of.bugs.KotlinPlugin");
+            jo.CallStatic("throw");
+    #else
+            Debug.LogWarning("Not on Android.");
+    #endif
+        }
+
+        public void ThrowKotlinOnBackground()
+        {
+    #if UNITY_ANDROID
+            var jo = new AndroidJavaObject("unity.of.bugs.KotlinPlugin");
+            jo.CallStatic("throwOnBackgroundThread");
+    #else
+            Debug.LogWarning("Not on Android.");
+    #endif
+        }
+
+        public void CrashNative()
+        {
+            // The method definition is missing
+            //crash();
+        }
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        private void MethodA() => throw new InvalidOperationException("Exception from A lady beetle 🐞");
+
+        // IL2CPP inlines this anyway
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        private void MethodB() => MethodA();
+
         // The method definition is missing
-        //crash();
+        //[DllImport("__Internal")]
+        //private static extern void crash();
     }
 
-    [MethodImpl(MethodImplOptions.NoInlining)]
-    private void MethodA() => throw new InvalidOperationException("Exception from A lady beetle 🐞");
-
-    // IL2CPP inlines this anyway
-    [MethodImpl(MethodImplOptions.NoInlining)]
-    private void MethodB() => MethodA();
-
-    // The method definition is missing
-    //[DllImport("__Internal")]
-    //private static extern void crash();
-}
-
-public class CustomException : System.Exception
-{
-    public CustomException(string message) : base(message)
+    public class CustomException : System.Exception
     {
+        public CustomException(string message) : base(message)
+        {
+        }
     }
-}
+ }
