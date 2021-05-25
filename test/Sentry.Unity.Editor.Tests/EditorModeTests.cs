3c69984bc7464dfa60300db118507b294d232e5c
bitsandfoxes <reg@bitfox.at>
2021-05-25 11:26:11 +0200

    Feat: Embedded link.xml in assembly (#194)
    * embedded link.xml
    
    * removed xml copy code
    
    * added changelog entry
    
    * embedded link.xml test
    
    * using cleanup
    
    * Update CHANGELOG.md
    
    * test refactor

--- test/Sentry.Unity.Editor.Tests/EditorModeTests.cs
+++ test/Sentry.Unity.Editor.Tests/EditorModeTests.cs
@@ -1,8 +1,6 @@
 ﻿using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using NUnit.Framework;
-using UnityEngine;
 using UnityEngine.TestTools;
 
 namespace Sentry.Unity.Editor.Tests
@@ -30,19 +28,6 @@ namespace Sentry.Unity.Editor.Tests
             // assert
             Assert.AreEqual(1, validationErrors.Count);
             Assert.NotNull(validationErrors.SingleOrDefault(e => e.PropertyName.Contains(nameof(SentryTestWindow.Options.Dsn))));
-        }
-
-        // This test method has a side effect of creating 'link.xml' if file doesn't exits.
-        [Test]
-        public void SentryTestWindow_OpenAndLinkXmlCopied_Successful()
-        {
-            LogAssert.ignoreFailingMessages = true; // mandatory
-
-            // Open & Close window to trigger 'link.xml' logic
-            SentryTestWindow.Open().Dispose();
-
-            var linkXmlPath = $"{Application.dataPath}/Resources/{SentryUnityOptions.ConfigRootFolder}/link.xml";
-            Assert.IsTrue(File.Exists(linkXmlPath));
         }
     }
 }
