1e5b36fb4c1fefd4659484f67af45ba7f88e2eb7
semuserable <semuserable@gmail.com>
2021-06-15 12:10:47 +0300

    Additional tests

---
+++ test/Sentry.Unity.Tests/DebouncerTests.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Sentry.Unity.Tests
+{
+    /// <summary>
+    /// Testing real in realtime.
+    /// </summary>
+    public sealed class DebouncerTests
+    {
+        private readonly TimeSpan DefaultOffset = TimeSpan.FromSeconds(1);
+
+        [UnityTest]
+        public IEnumerator LogTimeDebounce()
+        {
+            yield return AssertDefaultDebounce(new LogTimeDebounce(DefaultOffset));
+        }
+
+        [UnityTest]
+        public IEnumerator ErrorTimeDebounce()
+        {
+            yield return AssertDefaultDebounce(new ErrorTimeDebounce(DefaultOffset));
+        }
+
+        [UnityTest]
+        public IEnumerator WarningTimeDebounce()
+        {
+            yield return AssertDefaultDebounce(new WarningTimeDebounce(DefaultOffset));
+        }
+
+        private IEnumerator AssertDefaultDebounce(TimeDebounceBase debouncer)
+        {
+            // pass
+            Assert.IsTrue(debouncer.Debounced());
+
+            yield return new WaitForSeconds(0.5f);
+
+            // skip
+            Assert.IsFalse(debouncer.Debounced());
+
+            yield return new WaitForSeconds(0.4f);
+
+            // skip
+            Assert.IsFalse(debouncer.Debounced());
+
+            yield return new WaitForSeconds(0.1f);
+
+            // pass
+            Assert.IsTrue(debouncer.Debounced());
+        }
+    }
+}
--- test/Sentry.Unity.Tests/UnityApplicationLoggingIntegrationTests.cs
+++ test/Sentry.Unity.Tests/UnityApplicationLoggingIntegrationTests.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using System.Linq;
+using NUnit.Framework;
 using Sentry.Unity.Integrations;
 using Sentry.Unity.Tests.Stubs;
 using UnityEngine;
@@ -56,5 +57,42 @@ namespace Sentry.Unity.Tests
 
             Assert.AreEqual(2, hub.ConfigureScopeCalls.Count);
         }
+
+        [TestCaseSource(nameof(LogTypesAndSentryLevels))]
+        public void OnLogMessageReceived_UnityErrorLogTypes_CapturedAndCorrespondToSentryLevel(LogType unityLogType, SentryLevel sentryLevel)
+        {
+            var sut = _fixture.GetSut();
+            var hub = _fixture.Hub;
+            sut.Register(hub, SentryOptions);
+
+            sut.OnLogMessageReceived("condition", "stacktrace", unityLogType);
+
+            Assert.NotNull(hub.CapturedEvents.SingleOrDefault(capturedEvent => capturedEvent.Level == sentryLevel));
+        }
+
+        private static readonly object[] LogTypesAndSentryLevels =
+        {
+            new object[] { LogType.Error, SentryLevel.Error },
+            new object[] { LogType.Exception, SentryLevel.Error },
+            new object[] { LogType.Assert, SentryLevel.Error }
+        };
+
+        [TestCaseSource(nameof(LogTypesNotCaptured))]
+        public void OnLogMessageReceived_UnityNotErrorLogTypes_NotCaptured(LogType unityLogType)
+        {
+            var sut = _fixture.GetSut();
+            var hub = _fixture.Hub;
+            sut.Register(hub, SentryOptions);
+
+            sut.OnLogMessageReceived("condition", "stacktrace", unityLogType);
+
+            Assert.AreEqual(0, hub.CapturedEvents.Count);
+        }
+
+        private static readonly object[] LogTypesNotCaptured =
+        {
+            new object[] { LogType.Log },
+            new object[] { LogType.Warning }
+        };
     }
 }
