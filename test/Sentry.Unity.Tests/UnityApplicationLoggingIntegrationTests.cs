a149c0d3bf217a70a95ca54d5307cb909dd21537
semuserable <semuserable@gmail.com>
2021-06-16 20:58:37 +0300

    Fixture refactor

--- test/Sentry.Unity.Tests/UnityApplicationLoggingIntegrationTests.cs
+++ test/Sentry.Unity.Tests/UnityApplicationLoggingIntegrationTests.cs
@@ -10,64 +10,67 @@ namespace Sentry.Unity.Tests
     {
         private class Fixture
         {
-            public Stubs.TestHub Hub => new();
-            public IApplication Application => new TestApplication();
-
-            public UnityApplicationLoggingIntegration GetSut() => new(Application);
+            public UnityApplicationLoggingIntegration GetSut(IHub hub, SentryOptions sentryOptions)
+            {
+                var application = new TestApplication();
+                var integration = new UnityApplicationLoggingIntegration(application);
+                integration.Register(hub, sentryOptions);
+                return integration;
+            }
         }
 
-        private readonly Fixture _fixture = new();
+        private Fixture _fixture = null!;
+        private TestHub _hub = null!;
+        private SentryOptions _sentryOptions = null!;
 
-        private SentryOptions SentryOptions { get; set; } = new();
+        [SetUp]
+        public void SetUp()
+        {
+            _fixture = new Fixture();
+            _hub = new TestHub();
+            _sentryOptions = new SentryOptions();
+        }
 
         [Test]
         public void OnLogMessageReceived_WithError_CaptureEvent()
         {
-            var sut = _fixture.GetSut();
-            var hub = _fixture.Hub;
-            sut.Register(hub, SentryOptions);
+            var sut = _fixture.GetSut(_hub, _sentryOptions);
 
             sut.OnLogMessageReceived("condition", "stacktrace", LogType.Error);
 
-            Assert.AreEqual(1, hub.CapturedEvents.Count);
+            Assert.AreEqual(1, _hub.CapturedEvents.Count);
         }
 
         [Test]
         public void OnLogMessageReceived_WithSeveralErrorsDebounced_CaptureEvent()
         {
-            var sut = _fixture.GetSut();
-            var hub = _fixture.Hub;
-            sut.Register(hub, SentryOptions);
+            var sut = _fixture.GetSut(_hub, _sentryOptions);
 
             sut.OnLogMessageReceived("condition", "stacktrace", LogType.Error);
             sut.OnLogMessageReceived("condition", "stacktrace", LogType.Error);
 
-            Assert.AreEqual(1, hub.CapturedEvents.Count);
+            Assert.AreEqual(1, _hub.CapturedEvents.Count);
         }
 
         [Test]
         public void OnLogMessageReceived_Breadcrumbs_Added()
         {
-            var sut = _fixture.GetSut();
-            var hub = _fixture.Hub;
-            sut.Register(hub, SentryOptions);
+            var sut = _fixture.GetSut(_hub, _sentryOptions);
 
             sut.OnLogMessageReceived("condition", "stacktrace", LogType.Warning);
             sut.OnLogMessageReceived("condition", "stacktrace", LogType.Error);
 
-            Assert.AreEqual(2, hub.ConfigureScopeCalls.Count);
+            Assert.AreEqual(2, _hub.ConfigureScopeCalls.Count);
         }
 
         [TestCaseSource(nameof(LogTypesAndSentryLevels))]
         public void OnLogMessageReceived_UnityErrorLogTypes_CapturedAndCorrespondToSentryLevel(LogType unityLogType, SentryLevel sentryLevel)
         {
-            var sut = _fixture.GetSut();
-            var hub = _fixture.Hub;
-            sut.Register(hub, SentryOptions);
+            var sut = _fixture.GetSut(_hub, _sentryOptions);
 
             sut.OnLogMessageReceived("condition", "stacktrace", unityLogType);
 
-            Assert.NotNull(hub.CapturedEvents.SingleOrDefault(capturedEvent => capturedEvent.Level == sentryLevel));
+            Assert.NotNull(_hub.CapturedEvents.SingleOrDefault(capturedEvent => capturedEvent.Level == sentryLevel));
         }
 
         private static readonly object[] LogTypesAndSentryLevels =
@@ -80,13 +83,11 @@ namespace Sentry.Unity.Tests
         [TestCaseSource(nameof(LogTypesNotCaptured))]
         public void OnLogMessageReceived_UnityNotErrorLogTypes_NotCaptured(LogType unityLogType)
         {
-            var sut = _fixture.GetSut();
-            var hub = _fixture.Hub;
-            sut.Register(hub, SentryOptions);
+            var sut = _fixture.GetSut(_hub, _sentryOptions);
 
             sut.OnLogMessageReceived("condition", "stacktrace", unityLogType);
 
-            Assert.AreEqual(0, hub.CapturedEvents.Count);
+            Assert.AreEqual(0, _hub.CapturedEvents.Count);
         }
 
         private static readonly object[] LogTypesNotCaptured =
