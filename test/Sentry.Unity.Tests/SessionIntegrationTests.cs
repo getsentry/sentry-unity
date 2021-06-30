abbc501c74b4de99ebce803bfccdf32530d79dc9
bitsandfoxes <reg@bitfox.at>
2021-06-30 17:34:33 +0200

    initial session integration idea

---
+++ test/Sentry.Unity.Tests/SessionIntegrationTests.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections;
+using NUnit.Framework;
+using Sentry.Unity.Tests.Stubs;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Sentry.Unity.Tests
+{
+    public class SessionIntegrationTests
+    {
+        [UnityTest]
+        public IEnumerator SessionIntegration_SomethingElse()
+        {
+            yield return null;
+            var request = string.Empty;
+
+            using var _ = TestSentryUnity.Init(o =>
+            {
+                // o.AutoSessionTracking = true; We expect this to be true by default
+                o.AutoSessionTrackingInterval = TimeSpan.FromMilliseconds(10);
+            }, r =>
+            {
+                request = r.Content.ReadAsStringAsync().Result;
+                Debug.Log(request);
+            });
+
+            // var sentryGameObject = GameObject.Find("SentryMonoBehaviour");
+            // var sentryMonoBehaviour = sentryGameObject.GetComponent<SentryMonoBehaviour>();
+            //
+            // Debug.LogError("testerror");
+            //
+            // sentryMonoBehaviour.OnApplicationFocus(false);
+            // yield return new WaitForSeconds(1f);
+            // sentryMonoBehaviour.OnApplicationFocus(true);
+
+            Assert.AreNotEqual(string.Empty, request);
+        }
+    }
+}
