b381ed628cfd7d1801f54b1c40c8541175e4f9d0
bitsandfoxes <reg@bitfox.at>
2021-07-01 13:31:45 +0200

    added timeout

--- test/Sentry.Unity.Tests/SessionIntegrationTests.cs
+++ test/Sentry.Unity.Tests/SessionIntegrationTests.cs
@@ -1,8 +1,11 @@
 using System;
 using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
 using NUnit.Framework;
 using Sentry.Unity.Tests.Stubs;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.TestTools;
 
 namespace Sentry.Unity.Tests
@@ -30,28 +33,46 @@ namespace Sentry.Unity.Tests
         public IEnumerator SessionIntegration_SomethingElse()
         {
             yield return null;
-            var request = string.Empty;
+
+            var expectedRequestCount = 5;
+            var evt = new ManualResetEventSlim();
+            var requests = new List<string>();
 
             using var _ = TestSentryUnity.Init(o =>
             {
                 // o.AutoSessionTracking = true; We expect this to be true by default
                 o.AutoSessionTrackingInterval = TimeSpan.FromMilliseconds(10);
+                o.Debug = true;
+                o.DiagnosticLevel = SentryLevel.Info;
             }, r =>
             {
-                request = r.Content.ReadAsStringAsync().Result;
+                var request = r.Content.ReadAsStringAsync().GetAwaiter().GetResult();
                 Debug.Log(request);
+
+                requests.Add(request);
+
+                if (requests.Count >= expectedRequestCount)
+                {
+                    evt.Set();
+                }
             });
 
-            // var sentryGameObject = GameObject.Find("SentryMonoBehaviour");
-            // var sentryMonoBehaviour = sentryGameObject.GetComponent<SentryMonoBehaviour>();
-            //
-            // Debug.LogError("testerror");
-            //
-            // sentryMonoBehaviour.OnApplicationFocus(false);
-            // yield return new WaitForSeconds(1f);
-            // sentryMonoBehaviour.OnApplicationFocus(true);
+            var sentryGameObject = GameObject.Find("SentryMonoBehaviour");
+            var sentryMonoBehaviour = sentryGameObject.GetComponent<SentryMonoBehaviour>();
 
-            Assert.AreNotEqual(string.Empty, request);
+            sentryMonoBehaviour.OnApplicationFocus(false);
+            yield return new WaitForSeconds(1f);
+            sentryMonoBehaviour.OnApplicationFocus(true);
+
+            if (!evt.Wait(TimeSpan.FromSeconds(3)))
+            {
+                Assert.Fail("Timeout");
+            }
+
+            // TODO: Use the envelope to parse the request and check for session
+
+            Assert.AreEqual(expectedRequestCount, requests.Count);
         }
+
     }
 }
