cb469884366bf7c1e32546bea8bfb2b8d403f7bd
bitsandfoxes <reg@bitfox.at>
2021-07-01 14:42:02 +0200

    added 'all properties present' test for scriptable options

---
+++ test/Sentry.Unity.Editor.Tests/ScriptableSentryUnityOptionsTests.cs
@@ -0,0 +1,47 @@
+using System.IO;
+using NUnit.Framework;
+using UnityEditor;
+using UnityEngine;
+
+namespace Sentry.Unity.Editor.Tests
+{
+    public class ScriptableSentryUnityOptionsTests
+    {
+        [Test]
+        public void ScriptableSentryUnityOptions_Creation_AllPropertiesPresent()
+        {
+            const string testOptionsPath = "Assets/TestOptions.asset";
+
+            var scriptableOptions = ScriptableObject.CreateInstance<ScriptableSentryUnityOptions>();
+            AssetDatabase.CreateAsset(scriptableOptions, testOptionsPath);
+            AssetDatabase.SaveAssets();
+
+            var optionsAsString = File.ReadAllText(testOptionsPath);
+
+            StringAssert.Contains("Enabled", optionsAsString);
+            StringAssert.Contains("Dsn", optionsAsString);
+            StringAssert.Contains("CaptureInEditor", optionsAsString);
+            StringAssert.Contains("TracesSampleRate", optionsAsString);
+            StringAssert.Contains("AutoSessionTracking", optionsAsString);
+            StringAssert.Contains("AutoSessionTrackingInterval", optionsAsString);
+            StringAssert.Contains("ReleaseOverride", optionsAsString);
+            StringAssert.Contains("EnvironmentOverride", optionsAsString);
+            StringAssert.Contains("AttachStacktrace", optionsAsString);
+            StringAssert.Contains("MaxBreadcrumbs", optionsAsString);
+            StringAssert.Contains("ReportAssembliesMode", optionsAsString);
+            StringAssert.Contains("SendDefaultPii", optionsAsString);
+            StringAssert.Contains("IsEnvironmentUser", optionsAsString);
+            StringAssert.Contains("ServerNameOverride", optionsAsString);
+            StringAssert.Contains("EnableOfflineCaching", optionsAsString);
+            StringAssert.Contains("MaxCacheItems", optionsAsString);
+            StringAssert.Contains("InitCacheFlushTimeout", optionsAsString);
+            StringAssert.Contains("ShutdownTimeout", optionsAsString);
+            StringAssert.Contains("MaxQueueItems", optionsAsString);
+            StringAssert.Contains("Debug", optionsAsString);
+            StringAssert.Contains("DebugOnlyInEditor", optionsAsString);
+            StringAssert.Contains("DiagnosticLevel", optionsAsString);
+
+            AssetDatabase.DeleteAsset(testOptionsPath);
+        }
+    }
+}
