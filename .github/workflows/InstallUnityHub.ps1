354df19452da44156a570f22c398104bab77ea24
bitsandfoxes <reg@bitfox.at>
2021-07-19 13:10:09 +0200

    installation test

--- .github/workflows/InstallUnityHub.ps1
+++ .github/workflows/InstallUnityHub.ps1
@@ -1,1 +1,6 @@
-Write-Host "Hello World"
+
+Write-Host "Downloading Unity Hub"
+Invoke-RestMethod -Uri ${{ env.UNITY_HUB_URL }} -OutFile hub_installer.exe
+
+Write-Host "Installing Unity Hub"
+./hub_installer.exe /S
