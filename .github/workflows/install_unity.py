4073d4b49173b8172c645e4e28226928f3ef2a28
bitsandfoxes <reg@bitfox.at>
2021-07-19 18:58:50 +0200

    install unity via python

--- .github/workflows/install_unity.py
+++ .github/workflows/install_unity.py
@@ -3,9 +3,15 @@ from sys import stdout
 import subprocess
 import urllib.request
 
+print("Downloading the hub installer.")
 urllib.request.urlretrieve("https://public-cdn.cloud.unity3d.com/hub/prod/UnityHubSetup.exe", "hubinstaller.exe")
-subprocess.Popen(["hubinstaller.exe", "/S"], shell=True, stdout=subprocess.PIPE)
+print("Installer downloaded.")
 
+print("Installing the installer")
+subprocess.Popen(["hubinstaller.exe", "/S"], shell=True, stdout=subprocess.PIPE)
+print("Installer installed.")
+
+print("Installing Unity")
 hubpath = r'C:\\Program Files\\Unity Hub\\Unity Hub.exe'
 process = subprocess.Popen([hubpath, "--", "--headless",  "install", "--version", "2019.4.28f1", "-m", "android", "-m", "android-sdk-ndk-tools"], stdout=subprocess.PIPE)
 
