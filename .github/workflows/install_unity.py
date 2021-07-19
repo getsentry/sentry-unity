0e1f065fbd17a7a67285359b04c4cafa7b8b700f
bitsandfoxes <reg@bitfox.at>
2021-07-19 19:15:13 +0200

    install unity via python

--- .github/workflows/install_unity.py
+++ .github/workflows/install_unity.py
@@ -8,19 +8,23 @@ urllib.request.urlretrieve("https://public-cdn.cloud.unity3d.com/hub/prod/UnityH
 print("Installer downloaded.")
 
 print("Installing the installer")
-subprocess.Popen(["hubinstaller.exe", "/S"], shell=True, stdout=subprocess.PIPE)
+with open('hubinstaller.log', 'wb') as f: 
+    process = subprocess.Popen(["hubinstaller.exe", "/S"], shell=True, stdout=subprocess.PIPE
+    for c in iter(lambda: process.stdout.read(1), b''): 
+        sys.stdout.buffer.write(c)
+        f.buffer.write(c)
 print("Installer installed.")
 
-print("Installing Unity")
-hubpath = r'C:\\Program Files\\Unity Hub\\Unity Hub.exe'
-process = subprocess.Popen([hubpath, "--", "--headless",  "install", "--version", "2019.4.28f1", "-m", "android", "-m", "android-sdk-ndk-tools"], stdout=subprocess.PIPE)
+# print("Installing Unity")
+# hubpath = r'C:\\Program Files\\Unity Hub\\Unity Hub.exe'
+# process = subprocess.Popen([hubpath, "--", "--headless",  "install", "--version", "2019.4.28f1", "-m", "android", "-m", "android-sdk-ndk-tools"], stdout=subprocess.PIPE)
 
-while True:
-	output = process.stdout.readline().decode()
-	if output == '' and process.poll() is not None:
-		break
-	if output:
-		print(output, end =" ") # , end =" " so there are no double newlines 
+# while True:
+# 	output = process.stdout.readline().decode()
+# 	if output == '' and process.poll() is not None:
+# 		break
+# 	if output:
+# 		print(output, end =" ") # , end =" " so there are no double newlines 
 
-rc = process.poll()
-print(rc)
+# rc = process.poll()
+# print(rc)
