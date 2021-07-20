e93d8b56bbb4d4a7b5997937f28e9f90f5b28cfc
bitsandfoxes <reg@bitfox.at>
2021-07-20 16:34:03 +0200

    caching android build

--- .github/workflows/install_unity.py
+++ .github/workflows/install_unity.py
@@ -21,11 +21,15 @@ def install_unity():
 	print("\tversion:\t" + version)
 	print("\tchangeset:\t" + changeset)
 
-	process = subprocess.Popen([hubpath, "--", "--headless",  "install", "--version", version, "--changeset", changeset, "-m", "android", "-m", "android-sdk-ndk-tools"], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-	output, error = process.communicate()
+	process = subprocess.Popen([hubpath, "--", "--headless",  "install", "--version", version, "--changeset", changeset, "-m", "android", "-m", "android-sdk-ndk-tools"], stdout=subprocess.PIPE)
+	while True:
+		output = process.stdout.readline().decode().strip()
+		if output == '' and process.poll() is not None:
+			break
+		if output:
+			print(output)
 
-	print(output)
-	print(error)
+	return process.poll()
 
 def main():
 	download_hub()
