9a99d5c0bc1f1e27600258552ecba4be7a9f4214
bitsandfoxes <reg@bitfox.at>
2021-07-29 17:53:24 +0200

    added native options generation tests

--- test/Sentry.Unity.Editor.iOS.Tests/NativeOptionsTests.cs
+++ test/Sentry.Unity.Editor.iOS.Tests/NativeOptionsTests.cs
@@ -1,7 +1,43 @@
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using NUnit.Framework;
+using Debug = UnityEngine.Debug;
+
 namespace Sentry.Unity.Editor.iOS.Tests
 {
     public class NativeOptionsTests
     {
+        [Test]
+        public void GenerateOptions_NewSentryOptions_Compiles()
+        {
+            const string testOptionsFileName = "testOptions.m";
+            var nativeOptions = SentryNativeOptions.GenerateOptions(new SentryOptions());
+            File.WriteAllText(testOptionsFileName, nativeOptions);
 
+            var process = Process.Start("clang", $"-fsyntax-only -framework Foundation {testOptionsFileName}");
+            process.WaitForExit();
+
+            Assert.AreEqual(0, process.ExitCode);
+
+            File.Delete(testOptionsFileName);
+        }
+
+        [Test]
+        public void GenerateOptions_NewSentryOptionsGarbageAppended_FailsToCompiles()
+        {
+            const string testOptionsFileName = "testOptions.m";
+            var nativeOptions = SentryNativeOptions.GenerateOptions(new SentryOptions());
+            nativeOptions += "AppendedTextToFailCompilation";
+
+            File.WriteAllText(testOptionsFileName, nativeOptions);
+
+            var process = Process.Start("clang", $"-fsyntax-only -framework Foundation {testOptionsFileName}");
+            process.WaitForExit();
+
+            Assert.AreEqual(1, process.ExitCode);
+
+            File.Delete(testOptionsFileName);
+        }
     }
 }
