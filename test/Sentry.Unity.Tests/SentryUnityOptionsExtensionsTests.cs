44694fb3aa1f32ae46b265a3d4126eb9edf4e207
bitsandfoxes <stefan.jandl@sentry.io>
2021-08-17 17:24:33 +0200

    unity options extension tests (#274)

---
+++ test/Sentry.Unity.Tests/SentryUnityOptionsExtensionsTests.cs
@@ -0,0 +1,92 @@
+using NUnit.Framework;
+using Sentry.Unity.Tests.Stubs;
+
+namespace Sentry.Unity.Tests
+{
+    public class SentryUnityOptionsExtensionsTests
+    {
+        private class Fixture
+        {
+            public TestApplication TestApplication { get; set; } = new();
+            public bool Enabled { get; set; } = true;
+            public string Dsn { get; set; } = "http://test.com";
+            public bool CaptureInEditor { get; set; } = true;
+
+            public SentryUnityOptions GetSut() =>  new()
+            {
+                Enabled = Enabled,
+                Dsn = Dsn,
+                CaptureInEditor = CaptureInEditor
+            };
+        }
+
+        private Fixture _fixture = new();
+
+        [SetUp]
+        public void SetUp() => _fixture = new Fixture();
+
+        [Test]
+        public void ShouldInitializeSdk_OptionsIsNull_ReturnsFalse()
+        {
+            SentryUnityOptions? options = null;
+
+            var shouldInitialize = options.ShouldInitializeSdk();
+
+            Assert.IsFalse(shouldInitialize);
+        }
+
+        [Test]
+        public void ShouldInitializeSdk_OptionsDisabled_ReturnsFalse()
+        {
+            _fixture.Enabled = false;
+            var options = _fixture.GetSut();
+
+            var shouldInitialize = options.ShouldInitializeSdk();
+
+            Assert.IsFalse(shouldInitialize);
+        }
+
+        [Test]
+        public void ShouldInitializeSdk_NotCaptureInEditorAndApplicationIsEditor_ReturnsFalse()
+        {
+            _fixture.CaptureInEditor = false;
+            var options = _fixture.GetSut();
+
+            var shouldInitialize = options.ShouldInitializeSdk(_fixture.TestApplication);
+
+            Assert.IsFalse(shouldInitialize);
+        }
+
+        [Test]
+        public void ShouldInitializeSdk_DsnEmpty_ReturnsFalse()
+        {
+            _fixture.Dsn = string.Empty;
+            var options = _fixture.GetSut();
+
+            var shouldInitialize = options.ShouldInitializeSdk();
+
+            Assert.IsFalse(shouldInitialize);
+        }
+
+        [Test]
+        public void ShouldInitializeSdk_CorrectlyConfiguredForEditor_ReturnsTrue()
+        {
+            var options = _fixture.GetSut();
+
+            var shouldInitialize = options.ShouldInitializeSdk(_fixture.TestApplication);
+
+            Assert.IsTrue(shouldInitialize);
+        }
+
+        [Test]
+        public void ShouldInitializeSdk_CorrectlyConfigured_ReturnsTrue()
+        {
+            _fixture.TestApplication = new TestApplication(false);
+            var options = _fixture.GetSut();
+
+            var shouldInitialize = options.ShouldInitializeSdk(_fixture.TestApplication);
+
+            Assert.IsTrue(shouldInitialize);
+        }
+    }
+}
