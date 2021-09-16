785b43f5abd7653b7b6ff6305534142ed4dfc18e
bitsandfoxes <reg@bitfox.at>
2021-09-16 14:21:05 +0200

    removed init debug code

--- package-dev/Runtime/SentryInitialization.cs
+++ package-dev/Runtime/SentryInitialization.cs
@@ -24,21 +24,6 @@ namespace Sentry.Unity
 #endif
 
                 SentryUnity.Init(options);
-
-                Debug.Log("<color=red>Configuring the scope.</color>");
-                SentrySdk.ConfigureScope(scope =>
-                {
-                    scope.SetTag("my fancy unity tag", "my value");
-                    scope.User = new User
-                    {
-                        Id = "42",
-                        Email = "unity@bridge.awesome"
-                    };
-                    scope.SetExtra("test null extra", null);
-                    scope.SetExtra("test extra", "extra value");
-                });
-
-                SentrySdk.AddBreadcrumb(null, "Init Breadcrumb", null, "test", null, BreadcrumbLevel.Debug);
             }
         }
     }
