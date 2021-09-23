b4caf974b39dc97deabdda3f988c84585796d591
bitsandfoxes <stefan.jandl@sentry.io>
2021-09-23 20:17:54 +0200

    fix: Default values for new options (#333)

--- src/Sentry.Unity/ScriptableSentryUnityOptions.cs
+++ src/Sentry.Unity/ScriptableSentryUnityOptions.cs
@@ -44,8 +44,8 @@ namespace Sentry.Unity
         [field: SerializeField] internal float? SampleRate { get; set; }
         [field: SerializeField] internal int ShutdownTimeout { get; set; }
         [field: SerializeField] internal int MaxQueueItems { get; set; }
-        [field: SerializeField] internal bool IosNativeSupportEnabled { get; set; }
-        [field: SerializeField] internal bool AndroidNativeSupportEnabled { get; set; }
+        [field: SerializeField] internal bool IosNativeSupportEnabled { get; set; } = true;
+        [field: SerializeField] internal bool AndroidNativeSupportEnabled { get; set; } = true;
 
         [field: SerializeField] internal bool Debug { get; set; }
         [field: SerializeField] internal bool DebugOnlyInEditor { get; set; }
