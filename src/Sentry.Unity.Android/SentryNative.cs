8fdc856eb6ec4f0476ba628bb4a7abebf7a27fc9
Bruno Garcia <bruno@brunogarcia.com>
2021-09-24 19:39:15 -0400

    test

--- src/Sentry.Unity.Android/SentryNative.cs
+++ src/Sentry.Unity.Android/SentryNative.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Runtime.InteropServices;
 
 namespace Sentry.Unity.Android
@@ -20,13 +21,16 @@ namespace Sentry.Unity.Android
         /// Sentry's Android SDK initializes before Unity. But once Unity initializes, it takes the signal handler
         /// and does not forward the call to the original handler (sentry) before shutting down.
         /// This results in a crash captured by the Sentry Android SDK (Java/ART) layer captured crash report
-        /// containing a tompstone, without any of the scope data such as tags set through
+        /// containing a tombstone, without any of the scope data such as tags set through
         /// Sentry SDKs C# -> Java -> C
         /// </summary>
-        public static void ReinstallBackend() => sentry_reinstall_backend();
+        public static void ReinstallBackend() => ReinstallSentryNativeBackendStrategy();
 
         // libsentry.io
         [DllImport("sentry")]
         private static extern void sentry_reinstall_backend();
+
+        // Testing
+        internal static Action ReinstallSentryNativeBackendStrategy = sentry_reinstall_backend;
     }
 }
