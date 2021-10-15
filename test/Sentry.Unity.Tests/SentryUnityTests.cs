c2db1772c252425a601982c493e7763ebdd7e145
bitsandfoxes <reg@bitfox.at>
2021-10-15 16:30:40 +0200

    improved tests

--- test/Sentry.Unity.Tests/SentryUnityTests.cs
+++ test/Sentry.Unity.Tests/SentryUnityTests.cs
@@ -1,9 +1,49 @@
+using System;
 using NUnit.Framework;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.TestTools;
 
 namespace Sentry.Unity.Tests
 {
-    public class SentryUnityTests
+    [TestFixture]
+    public class SentryUnityTests : IPrebuildSetup, IPostBuildCleanup
     {
+        // If an options scriptable object exists Sentry SDK initializes itself on 'BeforeSceneLoad'.
+        // We check in prebuild if those options exist and are enabled, disable them and restore them on Cleanup
+        private ScriptableSentryUnityOptions? _optionsToRestore;
+
+        public void Setup()
+        {
+            var options = AssetDatabase.LoadAssetAtPath(ScriptableSentryUnityOptions.GetConfigPath(ScriptableSentryUnityOptions.ConfigName),
+                typeof(ScriptableSentryUnityOptions)) as ScriptableSentryUnityOptions;
+            if (options?.Enabled != true)
+            {
+                return;
+            }
+
+            Debug.Log("Sentry Options found: Disabling for the duration of the test.");
+            _optionsToRestore = options;
+            _optionsToRestore.Enabled = false;
+        }
+
+        public void Cleanup()
+        {
+            if (_optionsToRestore != null)
+            {
+                _optionsToRestore.Enabled = true;
+            }
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            if (SentrySdk.IsEnabled)
+            {
+                SentrySdk.Close();
+            }
+        }
+
         [Test]
         public void SentryUnity_OptionsValid_Initializes()
         {
