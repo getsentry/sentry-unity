e921ca983f24c9d6c040665fec5a2afe268c03c7
Simon Cropp <simon.cropp@gmail.com>
2021-11-03 10:56:40 +1100

    Update pack.ps1

--- scripts/pack.ps1
+++ scripts/pack.ps1
@@ -1,8 +1,8 @@
 New-Item "package-release" -ItemType Directory
 
 # Copy `package-dev` stuff
-dotnet tool install --global Alias --version 0.1.0-beta.4
-assemblyalias --target-directory "package-dev/Runtime"  --assemblies "Microsoft*;System*"
+dotnet tool install --global Alias --version 0.1.0-beta.5
+assemblyalias --target-directory "package-dev/Runtime" --assemblies-to-alias "Microsoft*;System*"
 
 Copy-Item "package-dev/*" -Destination "package-release/" -Exclude "README.md", "package.json", "Tests", "Tests.meta", "*.asmdef", "*.asmdef.meta", "SentryOptions.json*" -Recurse
 
