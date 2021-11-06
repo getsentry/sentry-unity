20afdf8f75267a2ea9954d0b60fc5ccf7aa32b77
Simon Cropp <simon.cropp@gmail.com>
2021-11-06 22:39:45 +1100

    Update pack.ps1

--- scripts/pack.ps1
+++ scripts/pack.ps1
@@ -1,8 +1,8 @@
 New-Item "package-release" -ItemType Directory
 
 # Copy `package-dev` stuff
-dotnet tool install --global Alias --version 0.1.0-beta.5
-assemblyalias --target-directory "package-dev/Runtime" --assemblies-to-alias "Microsoft*;System*" --key "src/sentry-dotnet/.assets/Sentry.snk"
+dotnet tool install --global Alias --version 0.1.0
+assemblyalias --target-directory "package-dev" --prefix "Sentry." --assemblies-to-alias "Microsoft*;System*" --key "src/sentry-dotnet/.assets/Sentry.snk"
 
 Copy-Item "package-dev/*" -Destination "package-release/" -Exclude "README.md", "package.json", "Tests", "Tests.meta", "*.asmdef", "*.asmdef.meta", "SentryOptions.json*" -Recurse
 
