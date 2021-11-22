bff62fa1a92ed3d8014537b275f452a0717fdc25
bitsandfoxes <reg@bitfox.at>
2021-11-22 17:38:45 +0100

    disabling debounce tests

--- test/Sentry.Unity.Tests/DebouncerTests.cs
+++ test/Sentry.Unity.Tests/DebouncerTests.cs
@@ -11,45 +11,48 @@ namespace Sentry.Unity.Tests
     /// </summary>
     public sealed class DebouncerTests
     {
-        private readonly TimeSpan DefaultOffset = TimeSpan.FromMilliseconds(100);
+        // Disabling because of flakiness
+        // https://github.com/getsentry/sentry-unity/issues/335
 
-        [UnityTest]
-        public IEnumerator LogTimeDebounce()
-        {
-            yield return AssertDefaultDebounce(new LogTimeDebounce(DefaultOffset));
-        }
-
-        [UnityTest]
-        public IEnumerator ErrorTimeDebounce()
-        {
-            yield return AssertDefaultDebounce(new ErrorTimeDebounce(DefaultOffset));
-        }
-
-        [UnityTest]
-        public IEnumerator WarningTimeDebounce()
-        {
-            yield return AssertDefaultDebounce(new WarningTimeDebounce(DefaultOffset));
-        }
-
-        private IEnumerator AssertDefaultDebounce(TimeDebounceBase debouncer)
-        {
-            // pass
-            Assert.IsTrue(debouncer.Debounced());
-
-            yield return new WaitForSeconds(0.050f);
-
-            // skip
-            Assert.IsFalse(debouncer.Debounced());
-
-            yield return new WaitForSeconds(0.02f);
-
-            // skip
-            Assert.IsFalse(debouncer.Debounced());
-
-            yield return new WaitForSeconds(0.04f);
-
-            // pass
-            Assert.IsTrue(debouncer.Debounced());
-        }
+        // private readonly TimeSpan DefaultOffset = TimeSpan.FromMilliseconds(100);
+        //
+        // [UnityTest]
+        // public IEnumerator LogTimeDebounce()
+        // {
+        //     yield return AssertDefaultDebounce(new LogTimeDebounce(DefaultOffset));
+        // }
+        //
+        // [UnityTest]
+        // public IEnumerator ErrorTimeDebounce()
+        // {
+        //     yield return AssertDefaultDebounce(new ErrorTimeDebounce(DefaultOffset));
+        // }
+        //
+        // [UnityTest]
+        // public IEnumerator WarningTimeDebounce()
+        // {
+        //     yield return AssertDefaultDebounce(new WarningTimeDebounce(DefaultOffset));
+        // }
+        //
+        // private IEnumerator AssertDefaultDebounce(TimeDebounceBase debouncer)
+        // {
+        //     // pass
+        //     Assert.IsTrue(debouncer.Debounced());
+        //
+        //     yield return new WaitForSeconds(0.050f);
+        //
+        //     // skip
+        //     Assert.IsFalse(debouncer.Debounced());
+        //
+        //     yield return new WaitForSeconds(0.02f);
+        //
+        //     // skip
+        //     Assert.IsFalse(debouncer.Debounced());
+        //
+        //     yield return new WaitForSeconds(0.04f);
+        //
+        //     // pass
+        //     Assert.IsTrue(debouncer.Debounced());
+        // }
     }
 }
