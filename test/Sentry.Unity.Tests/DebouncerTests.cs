c6e74bda8b4549576928a204a7582993d2ce36ae
bitsandfoxes <reg@bitfox.at>
2021-11-23 09:53:50 +0100

    ignore instead of comment

--- test/Sentry.Unity.Tests/DebouncerTests.cs
+++ test/Sentry.Unity.Tests/DebouncerTests.cs
@@ -9,50 +9,48 @@ namespace Sentry.Unity.Tests
     /// <summary>
     /// Testing debouncer in realtime.
     /// </summary>
+    [Ignore("Flakey")] // Ignoring because of flakiness: https://github.com/getsentry/sentry-unity/issues/335
     public sealed class DebouncerTests
     {
-        // Disabling because of flakiness
-        // https://github.com/getsentry/sentry-unity/issues/335
+        private readonly TimeSpan DefaultOffset = TimeSpan.FromMilliseconds(100);
 
-        // private readonly TimeSpan DefaultOffset = TimeSpan.FromMilliseconds(100);
-        //
-        // [UnityTest]
-        // public IEnumerator LogTimeDebounce()
-        // {
-        //     yield return AssertDefaultDebounce(new LogTimeDebounce(DefaultOffset));
-        // }
-        //
-        // [UnityTest]
-        // public IEnumerator ErrorTimeDebounce()
-        // {
-        //     yield return AssertDefaultDebounce(new ErrorTimeDebounce(DefaultOffset));
-        // }
-        //
-        // [UnityTest]
-        // public IEnumerator WarningTimeDebounce()
-        // {
-        //     yield return AssertDefaultDebounce(new WarningTimeDebounce(DefaultOffset));
-        // }
-        //
-        // private IEnumerator AssertDefaultDebounce(TimeDebounceBase debouncer)
-        // {
-        //     // pass
-        //     Assert.IsTrue(debouncer.Debounced());
-        //
-        //     yield return new WaitForSeconds(0.050f);
-        //
-        //     // skip
-        //     Assert.IsFalse(debouncer.Debounced());
-        //
-        //     yield return new WaitForSeconds(0.02f);
-        //
-        //     // skip
-        //     Assert.IsFalse(debouncer.Debounced());
-        //
-        //     yield return new WaitForSeconds(0.04f);
-        //
-        //     // pass
-        //     Assert.IsTrue(debouncer.Debounced());
-        // }
+        [UnityTest]
+        public IEnumerator LogTimeDebounce()
+        {
+            yield return AssertDefaultDebounce(new LogTimeDebounce(DefaultOffset));
+        }
+
+        [UnityTest]
+        public IEnumerator ErrorTimeDebounce()
+        {
+            yield return AssertDefaultDebounce(new ErrorTimeDebounce(DefaultOffset));
+        }
+
+        [UnityTest]
+        public IEnumerator WarningTimeDebounce()
+        {
+            yield return AssertDefaultDebounce(new WarningTimeDebounce(DefaultOffset));
+        }
+
+        private IEnumerator AssertDefaultDebounce(TimeDebounceBase debouncer)
+        {
+            // pass
+            Assert.IsTrue(debouncer.Debounced());
+
+            yield return new WaitForSeconds(0.050f);
+
+            // skip
+            Assert.IsFalse(debouncer.Debounced());
+
+            yield return new WaitForSeconds(0.02f);
+
+            // skip
+            Assert.IsFalse(debouncer.Debounced());
+
+            yield return new WaitForSeconds(0.04f);
+
+            // pass
+            Assert.IsTrue(debouncer.Debounced());
+        }
     }
 }
