8a006818cefdf1ef7842d94ea7e69ae05b6a727e
bitsandfoxes <reg@bitfox.at>
2021-11-24 12:37:22 +0100

    ignoring tests instead of testclass

--- test/Sentry.Unity.Tests/DebouncerTests.cs
+++ test/Sentry.Unity.Tests/DebouncerTests.cs
@@ -9,24 +9,26 @@ namespace Sentry.Unity.Tests
     /// <summary>
     /// Testing debouncer in realtime.
     /// </summary>
-    [Ignore("Flakey")] // Ignoring because of flakiness: https://github.com/getsentry/sentry-unity/issues/335
     public sealed class DebouncerTests
     {
         private readonly TimeSpan DefaultOffset = TimeSpan.FromMilliseconds(100);
 
         [UnityTest]
+        [Ignore("Ignored due to flakiness")] // Ignoring because of flakiness: https://github.com/getsentry/sentry-unity/issues/335
         public IEnumerator LogTimeDebounce()
         {
             yield return AssertDefaultDebounce(new LogTimeDebounce(DefaultOffset));
         }
 
         [UnityTest]
+        [Ignore("Ignored due to flakiness")] // Ignoring because of flakiness: https://github.com/getsentry/sentry-unity/issues/335
         public IEnumerator ErrorTimeDebounce()
         {
             yield return AssertDefaultDebounce(new ErrorTimeDebounce(DefaultOffset));
         }
 
         [UnityTest]
+        [Ignore("Ignored due to flakiness")] // Ignoring because of flakiness: https://github.com/getsentry/sentry-unity/issues/335
         public IEnumerator WarningTimeDebounce()
         {
             yield return AssertDefaultDebounce(new WarningTimeDebounce(DefaultOffset));
