c31b1637bb6d97f4cf0d47f29997365de669f6fa
Lucas Zimerman Fraulob <lucas-zimerman1@hotmail.com>
2021-12-08 11:23:57 -0300

    Simplified MacOS test

--- scripts/integration-test/SmokeTester.cs
+++ scripts/integration-test/SmokeTester.cs
@@ -13,16 +13,6 @@ public class SmokeTester : MonoBehaviour
         // On Android we'll grep logcat for this string instead of relying on exit code:
         Debug.Log("SMOKE TEST: PASS");
 
-        // Test passed: Exit Code 200 to avoid false positive from a graceful exit unrelated to this test run
-        // Create a file to write to.
-
-        if (Application.platform == RuntimePlatform.OSXEditor)
-        {
-            using (StreamWriter sw = File.CreateText("test.txt"))
-            {
-                sw.WriteLine("200");
-            }
-        }
         // Quit does not work on MacOS so we flag with a new file
         Application.Quit(200);
     }
