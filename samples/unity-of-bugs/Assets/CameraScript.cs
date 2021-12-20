a35f6f0d68c01becbc0aa9997484f5e3365f7e5d
Lucas Zimerman Fraulob <lucas-zimerman1@hotmail.com>
2021-12-20 17:54:43 -0300

    Add repro

---
+++ samples/unity-of-bugs/Assets/CameraScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Sentry;
+using System;
+
+public class CameraScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        try
+        {
+            //user code
+            throw new Exception("User camera script failed");
+        }
+        catch (Exception ex)
+        {
+            SentrySdk.CaptureException(ex);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
