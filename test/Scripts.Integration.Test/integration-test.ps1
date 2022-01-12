b9297496e3dabb8e323ab3e673d03e597e2d2607
Lucas Zimerman Fraulob <lucas-zimerman1@hotmail.com>
2022-01-12 12:16:06 -0300

    Ajust Unity Path on Github Actions

--- test/Scripts.Integration.Test/integration-test.ps1
+++ test/Scripts.Integration.Test/integration-test.ps1
@@ -1,5 +1,5 @@
 ﻿param($path)
-$path = "C:\2019.4.33f1\Editor"
+
 .\test/Scripts.Integration.Test/integration-create-project.ps1 "$path"
 .\test/Scripts.Integration.Test/integration-build-project.ps1 "$path"
 .\test/Scripts.Integration.Test/integration-update-sentry.ps1 "$path"
