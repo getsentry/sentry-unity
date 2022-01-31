b6c979df0bc6815076783b8256d2d3ebfc08640a
LucasZF <lucas-zimerman1@hotmail.com>
2022-01-31 09:56:52 -0300

    Setup Sentry before building. (#505)
    * Setup Sentry before building
    
    Co-authored-by: Sentry Github Bot <bot+github-bot@sentry.io>
    Co-authored-by: Bruno Garcia <bruno@brunogarcia.com>

---
+++ src/Sentry.Unity.Editor/UnityCommandLineArguments.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+
+namespace Sentry.Unity.Editor
+{
+    internal static class CommandLineArgumentParser
+    {
+        internal static Dictionary<string, string> Parse()
+        {
+            var commandLineArguments = new Dictionary<string, string>();
+            var args = Environment.GetCommandLineArgs();
+
+            for (int current = 0, next = 1; current < args.Length; current++, next++)
+            {
+                if (!args[current].StartsWith("-"))
+                {
+                    continue;
+                }
+
+                var flag = args[current].TrimStart('-');
+                var flagHasValue = next < args.Length && !args[next].StartsWith("-");
+                var flagValue = flagHasValue ? args[next].TrimStart('-') : "";
+
+                commandLineArguments.Add(flag, flagValue);
+            }
+            return commandLineArguments;
+        }
+    }
+}
