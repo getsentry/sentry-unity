ab7f941534369cec2f92dcd89c9cc0a7dadc787b
Lucas <lucas-zimerman1@hotmail.com>
2022-02-01 11:31:24 -0300

    Fix permission denied and split build from test

--- scripts/smoke-test-ios.ps1
+++ scripts/smoke-test-ios.ps1
@@ -1,3 +1,4 @@
+param($action)
 $ErrorActionPreference = "Stop"
 
 $XcodeArtifactPath = "samples/artifacts/builds/iOS/Xcode"
@@ -26,101 +27,133 @@ Class AppleDevice
     }
 }
 
-Write-Host -NoNewline "Applying SmokeTest flag on info.plist: "
-$smokeTestKey = "<key>RunSentrySmokeTest</key>"
-$infoPlist = (Get-Content -path "$XcodeArtifactPath/Info.plist" -Raw)
-If ($infoPlist -clike "*$smokeTestKey*")
+function Build()
 {
-    Write-Host "SKIPPED" -ForegroundColor Gray
-}
-Else 
-{        
-    $infoPlist.Replace("</dict>`n</plist>", "	$smokeTestKey`n	<string>True</string>`n</dict>`n</plist>") | Set-Content "$XcodeArtifactPath/Info.plist"
-    Write-Host "OK" -ForegroundColor Green
-}
 
-Write-Host "Building iOS project"
-$xCodeBuild =  Start-Process -FilePath "xcodebuild" -ArgumentList "-project", "$XcodeArtifactPath/$ProjectName.xcodeproj", "-scheme", "Unity-iPhone", "-configuration", "Release", "-sdk", "iphonesimulator", "-derivedDataPath", "$ArchievePath/$ProjectName" -PassThru
-$xCodeBuild.WaitForExit()
-If ($xCodeBuild.ExitCode -ne 0)
-{
-    Throw "Xcode build exited with code $($xCodeBuild.ExitCode)."
-}
-Write-Host "OK" -ForegroundColor Green
-
-Write-Host "Retrieving list of available simulators" -ForegroundColor Green
-# junk will contain the first item from the String that should be == Devices ==
-$deviceLabel , $iOSVersion, $deviceListRaw = xcrun simctl list devices
-[AppleDevice[]]$deviceList = @()
-$iOSVersion = $iOSVersion.Trim("-")
-foreach ($device in $deviceListRaw)
-{
-    If ($device.StartsWith("--"))
+    Write-Host -NoNewline "Applying SmokeTest flag on info.plist: "
+    $smokeTestKey = "<key>RunSentrySmokeTest</key>"
+    $infoPlist = (Get-Content -path "$XcodeArtifactPath/Info.plist" -Raw)
+    If ($infoPlist -clike "*$smokeTestKey*")
     {
-        # Reached at the end of the iOS list
-        break
-    }
-    $dev = [AppleDevice]::new()
-    $dev.Parse($device)
-    $deviceList += $dev
-}
-
-$deviceCount = $DeviceList.Count
-
-Write-Host "Found $deviceCount devices on version $iOSVersion" -ForegroundColor Green
-ForEach ($device in $deviceList)
-{
-    Write-Host "$($device.Name) - $($device.UUID)"
-}
-
-ForEach ($device in $deviceList)
-{
-    Write-Host "Starting Simulator $($device.Name) UUID $($device.UUID)" -ForegroundColor Green
-    xcrun simctl boot $($device.UUID)
-    Write-Host -NoNewline "Installing Smoke Test on $($device.Name): "
-    xcrun simctl install $($device.UUID) "$AppPath"
-    Write-Host "OK" -ForegroundColor Green
-    Write-Host "Launching SmokeTest on $($device.Name)" -ForegroundColor Green
-    $consoleOut = xcrun simctl launch --console-pty $($device.UUID) "io.sentry.samples.unityofbugs"
-    
-    Write-Host -NoNewline "Smoke test STATUS: "
-    $stdout = $consoleOut  | select-string 'SMOKE TEST: PASS'
-    If ($stdout -ne $null)
-    {
-        Write-Host "PASSED" -ForegroundColor Green
-        $device.TestPassed = $True
+        Write-Host "SKIPPED" -ForegroundColor Gray
     }
     Else 
-    {
-        Write-Host "FAILED" -ForegroundColor Red
-        Write-Host "$($device.Name) Console"
-        foreach ($consoleLine in $consoleOut)
-        {
-            Write-Host $consoleLine
-        }
-        Write-Host " ===== END OF CONSOLE ====="
-    }
-    Write-Host -NoNewline "Removing Smoke Test from $($device.Name): "
-    xcrun simctl uninstall $($device.UUID) "io.sentry.samples.unityofbugs"
-    Write-Host "OK" -ForegroundColor Green
-
-    Write-Host -NoNewline "Requesting shutdown for $($device.Name): "
-    # Do not wait for the Simulator to close and continue testing the other simulators.
-    Start-Process xcrun -ArgumentList "simctl shutdown `"$($device.UUID)`""
-    Write-Host "OK" -ForegroundColor Green
-}
-
-Write-Host "Test result"
-foreach ($device in $deviceList)
-{
-    Write-Host -NoNewline "$($device.Name) UUID $($device.UUID): "
-    If ($device.TestPassed)
-    {
-        Write-Host "PASSED" -ForegroundColor Green
-    }
-    else 
     {        
-        Write-Host "FAILED" -ForegroundColor Red
+        $infoPlist.Replace("</dict>`n</plist>", "	$smokeTestKey`n	<string>True</string>`n</dict>`n</plist>") | Set-Content "$XcodeArtifactPath/Info.plist"
+        Write-Host "OK" -ForegroundColor Green
     }
+
+    $mapFileParser = "MapFileParser.sh"
+    Write-Host -NoNewline "Fixing permission for $mapFileParser : "
+    $smokeTestKey = "<key>RunSentrySmokeTest</key>"
+    $infoPlist = (Get-Content -path "$XcodeArtifactPath/Info.plist" -Raw)
+    If (Test-Path -Path "$XcodeArtifactPath/$mapFileParser")
+    {
+        # We need to allow the execution of this script, otherwise Xcode will throw permission denied.
+        chmod +x "$XcodeArtifactPath/$mapFileParser"
+        Write-Host "OK" -ForegroundColor Green
+    }
+    Else 
+    {        
+        Write-Host "NOT FOUND" -ForegroundColor Gray
+    }
+
+    Write-Host "Building iOS project"
+    $xCodeBuild =  Start-Process -FilePath "xcodebuild" -ArgumentList "-project", "$XcodeArtifactPath/$ProjectName.xcodeproj", "-scheme", "Unity-iPhone", "-configuration", "Release", "-sdk", "iphonesimulator", "-derivedDataPath", "$ArchievePath/$ProjectName" -PassThru
+    $xCodeBuild.WaitForExit()
+    If ($xCodeBuild.ExitCode -ne 0)
+    {
+        Throw "Xcode build exited with code $($xCodeBuild.ExitCode)."
+    }
+    Write-Host "OK" -ForegroundColor Green
 }
-Write-Host "End of test."
+
+function Test
+{
+    Write-Host "Retrieving list of available simulators" -ForegroundColor Green
+    # junk will contain the first item from the String that should be == Devices ==
+    $deviceLabel , $iOSVersion, $deviceListRaw = xcrun simctl list devices
+    [AppleDevice[]]$deviceList = @()
+    $iOSVersion = $iOSVersion.Trim("-")
+    foreach ($device in $deviceListRaw)
+    {
+        If ($device.StartsWith("--"))
+        {
+            # Reached at the end of the iOS list
+            break
+        }
+        $dev = [AppleDevice]::new()
+        $dev.Parse($device)
+        $deviceList += $dev
+    }
+
+    $deviceCount = $DeviceList.Count
+
+    Write-Host "Found $deviceCount devices on version $iOSVersion" -ForegroundColor Green
+    ForEach ($device in $deviceList)
+    {
+        Write-Host "$($device.Name) - $($device.UUID)"
+    }
+
+    ForEach ($device in $deviceList)
+    {
+        Write-Host "Starting Simulator $($device.Name) UUID $($device.UUID)" -ForegroundColor Green
+        xcrun simctl boot $($device.UUID)
+        Write-Host -NoNewline "Installing Smoke Test on $($device.Name): "
+        xcrun simctl install $($device.UUID) "$AppPath"
+        Write-Host "OK" -ForegroundColor Green
+        Write-Host "Launching SmokeTest on $($device.Name)" -ForegroundColor Green
+        $consoleOut = xcrun simctl launch --console-pty $($device.UUID) "io.sentry.samples.unityofbugs"
+        
+        Write-Host -NoNewline "Smoke test STATUS: "
+        $stdout = $consoleOut  | select-string 'SMOKE TEST: PASS'
+        If ($stdout -ne $null)
+        {
+            Write-Host "PASSED" -ForegroundColor Green
+            $device.TestPassed = $True
+        }
+        Else 
+        {
+            Write-Host "FAILED" -ForegroundColor Red
+            Write-Host "$($device.Name) Console"
+            foreach ($consoleLine in $consoleOut)
+            {
+                Write-Host $consoleLine
+            }
+            Write-Host " ===== END OF CONSOLE ====="
+        }
+        Write-Host -NoNewline "Removing Smoke Test from $($device.Name): "
+        xcrun simctl uninstall $($device.UUID) "io.sentry.samples.unityofbugs"
+        Write-Host "OK" -ForegroundColor Green
+
+        Write-Host -NoNewline "Requesting shutdown for $($device.Name): "
+        # Do not wait for the Simulator to close and continue testing the other simulators.
+        Start-Process xcrun -ArgumentList "simctl shutdown `"$($device.UUID)`""
+        Write-Host "OK" -ForegroundColor Green
+    }
+
+    Write-Host "Test result"
+    foreach ($device in $deviceList)
+    {
+        Write-Host -NoNewline "$($device.Name) UUID $($device.UUID): "
+        If ($device.TestPassed)
+        {
+            Write-Host "PASSED" -ForegroundColor Green
+        }
+        else 
+        {        
+            Write-Host "FAILED" -ForegroundColor Red
+        }
+    }
+    Write-Host "End of test."
+}
+$action = "Build"
+
+If ($action -eq $null -or $action -eq "Build")
+{
+    Build
+}
+If ($action -eq $null -or $action -eq "Test")
+{
+    Test
+}
