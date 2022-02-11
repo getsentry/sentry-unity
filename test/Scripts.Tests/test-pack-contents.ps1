6de1c21d18210cb1bfeafc99b5b6f25d6e7c7848
Ivan Dlugos <6349682+vaind@users.noreply.github.com>
2022-02-11 19:07:25 +0100

    Regression: collect android native libraries as an artifact (#556)
    * ci: collect android native libraries as an artifact
    
    * ci: ensure android native libs are part of the package

--- test/Scripts.Tests/test-pack-contents.ps1
+++ test/Scripts.Tests/test-pack-contents.ps1
@@ -42,6 +42,23 @@ try {
         $result | Format-Table -AutoSize
         exit 3
     }
-} finally{
+} finally {
     $zip.Dispose()
+}
+
+$androidLibsDir = "$projectRoot/modules/sentry-java/sentry-android-ndk/build/intermediates/merged_native_libs/release/out/lib/"
+if (-not(Test-Path -Path $androidLibsDir)) {
+    Write-Host  "Android native libs not found in: '$androidLibsDir'"
+    exit 1
+}
+
+$androidLibs = Get-ChildItem -Recurse $androidLibsDir | ForEach-Object {$_.Directory.Name + "/" + $_.Name}
+$result = Compare-Object $androidLibs (Get-Content "$PSScriptRoot/android-libs.snapshot")
+if ($result.count -eq 0) {
+    Write-Host  "Android native libs match snapshot."
+}
+else {
+    Write-Host  "Android native libs do not match snapshot."
+    $result | Format-Table -AutoSize
+    exit 3
 }
