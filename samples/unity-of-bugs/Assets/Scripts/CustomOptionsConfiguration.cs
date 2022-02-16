67480986185a9c353f6526378559db66c7aa906c
Stefan Jandl <reg@bitfox.at>
2022-02-16 18:06:55 +0100

    Update samples/unity-of-bugs/Assets/Scripts/CustomOptionsConfiguration.cs
    Co-authored-by: Bruno Garcia <bruno@brunogarcia.com>

--- samples/unity-of-bugs/Assets/Scripts/CustomOptionsConfiguration.cs
+++ samples/unity-of-bugs/Assets/Scripts/CustomOptionsConfiguration.cs
@@ -7,8 +7,9 @@ public class CustomOptionsConfiguration : ScriptableOptionsConfiguration
     // This method gets called when you instantiated the scriptable object and added it to the configuration window
     public override void Configure(SentryUnityOptions options)
     {
-        // NOTE: You have complete access to the options object here but changes to the options will not make it
-        // to the native layer because the native layer is configured during build time.
+        // NOTE: Changes to the options object done here will not affect native crashes. The native SDKs only take 
+        // options defined in the Sentry editor configuration window. 
+        // Learn more at: https://docs.sentry.io/platforms/unity/native-support/configuration/
 
         options.BeforeSend = sentryEvent =>
         {
