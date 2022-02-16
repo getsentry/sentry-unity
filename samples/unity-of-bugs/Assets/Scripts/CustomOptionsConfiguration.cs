80da2e626eca955f8652ae0083ca6f598bf9c063
bitsandfoxes <reg@bitfox.at>
2022-02-16 12:26:45 +0100

    added additional comment

--- samples/unity-of-bugs/Assets/Scripts/CustomOptionsConfiguration.cs
+++ samples/unity-of-bugs/Assets/Scripts/CustomOptionsConfiguration.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Assets/Resources/Sentry/CustomOptionsConfiguration", menuName = "Sentry/CustomOptionsConfiguration", order = 999)]
 public class CustomOptionsConfiguration : ScriptableOptionsConfiguration
 {
+    // This method gets called when you instantiated the scriptable object and added it to the configuration window
     public override void Configure(SentryUnityOptions options)
     {
         // NOTE: You have complete access to the options object here but changes to the options will not make it
