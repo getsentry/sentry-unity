e1a117e30aa67f20325c2c44d59a31172254e835
Ivan Dlugos <dlugos.ivan@gmail.com>
2022-02-25 15:44:44 +0100

    ci: fix windows

--- .github/workflows/ci-env.ps1
+++ .github/workflows/ci-env.ps1
@@ -6,13 +6,13 @@ param (
 switch ($name) {
     "unity2019" {
         return "2019.4.34f1"
-     }
-     "unity2020" {
+    }
+    "unity2020" {
         return "2020.3.27f1"
-     }
-     "unity2021" {
+    }
+    "unity2021" {
         return "2021.2.10f1"
-     }
+    }
     Default {
         throw "Unkown variable '$name'"
     }
