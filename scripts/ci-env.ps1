f7ba81b3264d417456f37b02109060fa66f6cb6d
vaind <vaind@users.noreply.github.com>
2022-04-07 03:10:43 +0000

    chore: update to Unity 2021.2.19f1

--- scripts/ci-env.ps1
+++ scripts/ci-env.ps1
@@ -11,7 +11,7 @@ switch ($name) {
         return "2020.3.32f1"
     }
     "unity2021" {
-        return "2021.2.18f1"
+        return "2021.2.19f1"
     }
     Default {
         throw "Unkown variable '$name'"
