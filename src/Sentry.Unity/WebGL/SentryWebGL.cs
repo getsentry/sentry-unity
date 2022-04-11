d233371f3e78b8395394db067ff53ee26b7b1725
Ivan Dlugos <dlugos.ivan@gmail.com>
2022-04-11 10:25:45 +0200

    fix: WebGL BackgroundWorker implementation & sample packages

--- src/Sentry.Unity/WebGL/SentryWebGL.cs
+++ src/Sentry.Unity/WebGL/SentryWebGL.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 using Sentry.Extensibility;
 using Sentry.Infrastructure;
 using Sentry.Internal;
+using Sentry.Internal.Http;
 using Sentry.Protocol.Envelopes;
 using UnityEngine;
 using UnityEngine.Networking;
@@ -28,6 +29,9 @@ namespace Sentry.Unity.WebGL
 
             // Caching transport relies on a background thread
             options.CacheDirectoryPath = null;
+            // Note: we need to use a custom background worker which actually doesn't work in the background
+            // because Unity doesn't support async (multithreading) yet. This may change in the future so let's watch
+            // https://docs.unity3d.com/2019.4/Documentation/ScriptReference/PlayerSettings.WebGL-threadsSupport.html
             options.BackgroundWorker = new WebBackgroundWorker(options, SentryMonoBehaviour.Instance);
 
             // Still cant' find out what's using Threads so:
@@ -53,32 +57,22 @@ namespace Sentry.Unity.WebGL
         {
             _options = options;
             _behaviour = behaviour;
-            // var composer = new SdkComposer(options);
-            // HTTP transport is not compatible. Need to use Unity's one.
-            // _transport = composer.CreateTransport();
         }
 
         public bool EnqueueEnvelope(Envelope envelope)
         {
-            // _transport.SendEnvelopeAsync(envelope, CancellationToken.None)
-            //     .ContinueWith(r => _options.DiagnosticLogger?.LogInfo("Result of envelope capture was: {0}", r.Status));
             _ = _behaviour.StartCoroutine(SendEnvelope(envelope));
             return true;
         }
 
         private IEnumerator SendEnvelope(Envelope envelope)
         {
-            var dsn = Dsn.Parse(_options.Dsn!);
-            var authHeader =
-                $"Sentry sentry_version={Sentry.Constants.ProtocolVersion}," +
-                $"sentry_client={UnitySdkInfo.Name}/{UnitySdkInfo.Version}," +
-                $"sentry_key={dsn.PublicKey}," +
-                (dsn.SecretKey is { } secretKey ? $"sentry_secret={secretKey}," : null) +
-                $"sentry_timestamp={_clock.GetUtcNow().ToUnixTimeSeconds()}";
-
-            var www = new UnityWebRequest(dsn.GetEnvelopeEndpointUri());
-            www.method = "POST";
-            www.SetRequestHeader("X-Sentry-Auth", authHeader);
+            var builder = new HttpRequestBuilder(_options);
+            var www = new UnityWebRequest();
+            www.url = builder.GetEnvelopeEndpointUri().ToString();
+            www.method = UnityWebRequest.kHttpVerbPOST;
+            www.SetRequestHeader(builder.AuthHeaderName, builder.AuthHeader(_clock.GetUtcNow()));
+            // TODO is it OK to call .Wait() here in webGL?
             var stream = new MemoryStream();
             envelope.SerializeAsync(stream, _options.DiagnosticLogger).Wait(TimeSpan.FromSeconds(2));
             stream.Flush();
@@ -86,19 +80,12 @@ namespace Sentry.Unity.WebGL
             www.downloadHandler = new DownloadHandlerBuffer();
             yield return www.SendWebRequest();
 
-            while (!www.isDone)
+            if (www.isNetworkError || www.isHttpError || www.responseCode != 200)
             {
-                yield return null;
+                _options.DiagnosticLogger?.LogWarning("error sending request to Sentry: {0}", www.error);
             }
-            if (
-                www.isNetworkError || www.isHttpError
-                                   || www.responseCode != 200)
-            {
-                _options.DiagnosticLogger?.LogWarning("error sending request to sentry: {0}", www.error);
-            }
-            {
-                _options.DiagnosticLogger?.LogDebug("Sentry sent back: {0}", www.downloadHandler.text);
-            }
+
+            _options.DiagnosticLogger?.LogDebug("Sentry sent back: {0}", www.downloadHandler.text);
         }
 
         public Task FlushAsync(TimeSpan timeout) => Task.CompletedTask;
