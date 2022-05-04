786b15db6403353148beaf2242b63ab378247ecb
Ivan Dlugos <6349682+vaind@users.noreply.github.com>
2022-05-04 14:34:35 +0200

    Update scripts/unity.ps1
    Co-authored-by: Stefan Jandl <stefan.jandl@sentry.io>

--- scripts/unity.ps1
+++ scripts/unity.ps1
@@ -3,5 +3,5 @@ $ErrorActionPreference = "Stop"
 
 . $PSScriptRoot/unity-utils.ps1
 
-# Redirecting the output to $null because RunUnity prints logs using Write-Host AND Write-Output - we don't need both here.
+Log output is written to 'unity.log'
 RunUnity $args[0] ($args | Select-Object -Skip 1)
