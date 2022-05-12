297acd06d6a82417c6f81448cb8a837b47360c4d
bitsandfoxes <reg@bitfox.at>
2022-05-12 15:46:51 +0200

    hooks

---
+++ samples/unity-of-bugs/Assets/Editor/CompileInterceptor.cs
@@ -0,0 +1,39 @@
+﻿using UnityEditor;
+using UnityEditor.Compilation;
+using UnityEngine;
+
+[InitializeOnLoad]
+public class CompileInterceptor
+{
+    static CompileInterceptor()
+    {
+        CompilationPipeline.compilationStarted -= OnCompilationStarted;
+        CompilationPipeline.compilationStarted += OnCompilationStarted;
+
+        CompilationPipeline.assemblyCompilationStarted -= OnAssemblyCompilationStarted;
+        CompilationPipeline.assemblyCompilationStarted += OnAssemblyCompilationStarted;
+
+        CompilationPipeline.assemblyCompilationFinished -= OnAssemblyCompilationFinished;
+        CompilationPipeline.assemblyCompilationFinished += OnAssemblyCompilationFinished;
+
+        CompilationPipeline.compilationFinished -= OnCompilationFinished;
+        CompilationPipeline.compilationFinished += OnCompilationFinished;
+    }
+
+    private static void OnCompilationStarted(object o) => Debug.Log("Compilation started.");
+    private static void OnAssemblyCompilationStarted(string assemblyPath) =>
+        Debug.Log($"Starting to compile: {assemblyPath}");
+
+    private static void OnAssemblyCompilationFinished(string assemblyPath, CompilerMessage[] compilerMessages)
+    {
+        if (assemblyPath.Contains("Assembly-CSharp.dll"))
+        {
+            Debug.Log($"Finished compiling: {assemblyPath}");
+
+            // This is running on the main thread
+            Debug.Log("This is where IL Weaving will happen.");
+        }
+    }
+
+    private static void OnCompilationFinished(object o) => Debug.Log("Compilation finished.");
+}
