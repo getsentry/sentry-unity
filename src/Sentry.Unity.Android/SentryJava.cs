2f9c455cd010630e0cbdf85e2c7d4440d2d3b490
Ivan Dlugos <dlugos.ivan@gmail.com>
2022-05-21 14:05:26 +0200

    feat: Android (java) context sync

--- src/Sentry.Unity.Android/SentryJava.cs
+++ src/Sentry.Unity.Android/SentryJava.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 namespace Sentry.Unity.Android
@@ -57,5 +58,103 @@ namespace Sentry.Unity.Android
 
         private static bool Attach() => AndroidJNI.AttachCurrentThread() == 0;
         private static AndroidJavaObject GetSentryJava() => new AndroidJavaClass("io.sentry.Sentry");
+
+        public static void WriteScope(
+            int? GpuId,
+            string? GpuName,
+            string? GpuVendorName,
+            int? GpuMemorySize,
+            string? GpuNpotSupport,
+            string? GpuVersion,
+            string? GpuApiType,
+            int? GpuMaxTextureSize,
+            bool? GpuSupportsDrawCallInstancing,
+            bool? GpuSupportsRayTracing,
+            bool? GpuSupportsComputeShaders,
+            bool? GpuSupportsGeometryShaders,
+            string? GpuVendorId,
+            bool? GpuMultiThreadedRendering,
+            string? GpuGraphicsShaderLevel)
+        {
+            if (Attach())
+            {
+                using var gpu = new AndroidJavaObject("io.sentry.protocol.Gpu");
+                gpu.SetIfNotNull("name", GpuName);
+                gpu.SetIfNotNull("id", GpuId);
+                int intVendorId;
+                if (GpuVendorId is not null && int.TryParse(GpuVendorId, out intVendorId) && intVendorId != 0)
+                {
+                    using var integer = new AndroidJavaObject("java.lang.Integer", intVendorId);
+                    gpu.Set("vendorId", integer);
+                }
+                gpu.SetIfNotNull("vendorName", GpuVendorName);
+                gpu.SetIfNotNull("memorySize", GpuMemorySize);
+                gpu.SetIfNotNull("apiType", GpuApiType);
+                gpu.SetIfNotNull("multiThreadedRendering", GpuMultiThreadedRendering);
+                gpu.SetIfNotNull("version", GpuVersion);
+                gpu.SetIfNotNull("npotSupport", GpuNpotSupport);
+
+                using var sentry = GetSentryJava();
+                sentry.CallStatic("configureScope", new ScopeCallback(scope =>
+                {
+                    using var contexts = scope.Call<AndroidJavaObject>("getContexts");
+                    contexts.Call("setGpu", gpu);
+                }));
+            }
+        }
+
+        private static void SetIfNotNull<T>(this AndroidJavaObject javaObject, string property, T? value, string? valueClass = null)
+        {
+            if (value is not null)
+            {
+                if (valueClass is null)
+                {
+                    javaObject.Set(property, value!);
+                }
+                else
+                {
+                    using var valueObject = new AndroidJavaObject(valueClass, value!);
+                    javaObject.Set(property, valueObject);
+                }
+            }
+        }
+
+        private static void SetIfNotNull(this AndroidJavaObject javaObject, string property, int? value) =>
+            SetIfNotNull(javaObject, property, value, "java.lang.Integer");
+
+        private static void SetIfNotNull(this AndroidJavaObject javaObject, string property, bool? value) =>
+            SetIfNotNull(javaObject, property, value, "java.lang.Boolean");
+
+        /// Implements the io.sentry.ScopeCallback interface.
+        internal class ScopeCallback : AndroidJavaProxy
+        {
+            private readonly Action<AndroidJavaObject> _callback;
+
+            public ScopeCallback(Action<AndroidJavaObject> callback) : base("io.sentry.ScopeCallback")
+            {
+                _callback = callback;
+            }
+
+            // Note: defining the method should be enough with the default Invoke(), but in reality it doesn't work:
+            // No such proxy method: Sentry.Unity.Android.SentryJava+ScopeCallback.run(UnityEngine.AndroidJavaObject)
+            //   public void run(AndroidJavaObject scope) => UnityEngine.Debug.Log("run() invoked");
+            // Therefore, we're overriding the Invoke() instead:
+            public override AndroidJavaObject? Invoke(string methodName, AndroidJavaObject[] args)
+            {
+                try
+                {
+                    if (methodName != "run" || args.Length != 1)
+                    {
+                        throw new Exception($"Invalid invocation: {methodName}({args.Length} args)");
+                    }
+                    _callback(args[0]);
+                }
+                catch (Exception e)
+                {
+                    UnityEngine.Debug.LogError($"Error in SentryJava.ScopeCallback: {e}");
+                }
+                return null;
+            }
+        }
     }
 }
