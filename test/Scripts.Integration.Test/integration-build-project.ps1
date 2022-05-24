568583b4c1806e294145d4fe3859839a5829b63d
Ivan Dlugos <dlugos.ivan@gmail.com>
2022-05-24 17:46:57 +0200

    chore: fix integration tests in CI

--- test/Scripts.Integration.Test/integration-build-project.ps1
+++ test/Scripts.Integration.Test/integration-build-project.ps1
@@ -1,6 +1,7 @@
 ﻿param(
     [string] $UnityPath,
-    [string] $Platform = ""
+    [string] $Platform = "",
+    [Switch] $CheckSymbols
 )
 
 . ./test/Scripts.Integration.Test/IntegrationGlobals.ps1
@@ -11,10 +12,16 @@ $buildMethod = BuildMethodFor $Platform
 $outputPath = "$NewProjectBuildPath/$(GetTestAppName $buildMethod)"
 
 Write-Host -NoNewline "Executing ${buildMethod}:"
-$symbolServerOutput = RunWithSymbolServer -Callback {
-    RunUnityCustom $unityPath @("-batchmode", "-projectPath ", "$NewProjectPath", `
-            "-executeMethod", $buildMethod , "-buildPath", $outputPath, "-quit")
+$unityArgs = @("-batchmode", "-projectPath ", "$NewProjectPath", "-executeMethod", $buildMethod , "-buildPath", $outputPath, "-quit")
+
+if ($CheckSymbols)
+{
+    $symbolServerOutput = RunWithSymbolServer -Callback { RunUnityCustom $unityPath $unityArgs }
+    CheckSymbolServerOutput $buildMethod $symbolServerOutput
 }
-CheckSymbolServerOutput $buildMethod $symbolServerOutput
+else
+{
+    RunUnityCustom $unityPath $unityArgs
+}
 Write-Host "Project built successfully" -ForegroundColor Green
 Get-ChildItem $NewProjectBuildPath
