17f775e731e25899871090d87f129e118302afe6
bitsandfoxes <reg@bitfox.at>
2022-06-15 16:10:37 +0200

    fixed integration-test

--- test/Scripts.Integration.Test/integration-test.ps1
+++ test/Scripts.Integration.Test/integration-test.ps1
@@ -1,7 +1,8 @@
-﻿param($path)
+﻿param(
+    [string] $UnityPath
+)
 
-.\test/Scripts.Integration.Test/create-project.ps1 "$path"
-.\test/Scripts.Integration.Test/build-project.ps1 "$path"
-.\test/Scripts.Integration.Test/update-sentry.ps1 "$path"
-.\test/Scripts.Integration.Test/build-project.ps1 "$path"
-.\test/Scripts.Integration.Test/run-smoke-test.ps1
+.\test/Scripts.Integration.Test/create-project.ps1 "$UnityPath"
+.\test/Scripts.Integration.Test/update-sentry.ps1 "$UnityPath"
+.\test/Scripts.Integration.Test/build-project.ps1 "$UnityPath"
+.\test/Scripts.Integration.Test/run-smoke-test.ps1 -Smoke
