a94a86d2aec2e20747c3d9a59a71b15c3e3aa00b
Ivan Dlugos <dlugos.ivan@gmail.com>
2022-06-23 19:15:11 +0200

    test: async stack trace (wip)

--- test/Sentry.Unity.Tests/SentryUnityTests.cs
+++ test/Sentry.Unity.Tests/SentryUnityTests.cs
@@ -3,6 +3,14 @@ using NUnit.Framework;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.TestTools;
+using System.IO;
+using System.Text;
+using System.Text.Json;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Debug = UnityEngine.Debug;
+using Sentry.Extensibility;
 
 namespace Sentry.Unity.Tests
 {
@@ -41,6 +49,55 @@ namespace Sentry.Unity.Tests
             if (SentrySdk.IsEnabled)
             {
                 SentrySdk.Close();
+            }
+        }
+
+        [Test]
+        public void AsyncStackTrace()
+        {
+            var options = new SentryUnityOptions();
+            options.AttachStacktrace = true;
+            options.StackTraceMode = StackTraceMode.Original;
+            var sut = new SentryStackTraceFactory(options);
+
+            IList<SentryStackFrame> framesSentry = null!;
+            StackFrame[] framesManual = null!;
+            Task.Run(() =>
+            {
+                var stackTrace = new StackTrace(true);
+                framesManual = stackTrace.GetFrames();
+
+                var sentryStackTrace = sut.Create()!;
+                var framesReversed = new System.Collections.Generic.List<SentryStackFrame>(sentryStackTrace.Frames);
+                framesReversed.Reverse();
+                framesSentry = framesReversed;
+                return 42; // returning a value here messes up a stack trace
+            }).Wait();
+
+            Debug.Log("Manually captured stack trace:");
+            foreach (var frame in framesManual)
+            {
+                Debug.Log($"  {frame.GetMethod()?.DeclaringType?.FullName} in {frame}");
+            }
+
+            Debug.Log("");
+
+            Debug.Log("Sentry captured stack trace:");
+            foreach (var frame in framesSentry)
+            {
+                Debug.Log($"  {frame.Module} in {frame.Function}");
+            }
+
+            Debug.Log("");
+
+            Debug.Log("Sentry captured stack trace (JSON):");
+            foreach (var frame in framesSentry)
+            {
+                using var stream = new MemoryStream();
+                using var writer = new Utf8JsonWriter(stream);
+                frame.WriteTo(writer, null);
+                writer.Flush();
+                Debug.Log($"  {(Encoding.UTF8.GetString(stream.ToArray()))}");
             }
         }
 
