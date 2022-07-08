48d31d1bb548a1b5bb841a9e413731e6618cafc5
bitsandfoxes <reg@bitfox.at>
2022-07-08 19:14:52 +0200

    added CppPlugin because it needs the meta anyway

---
+++ test/Scripts.Integration.Test/Scripts/CppPlugin.cpp
@@ -0,0 +1,24 @@
+#include <exception>
+#include <iostream>
+#include <string>
+
+extern "C" {
+void crash_in_cpp()
+{
+    char *ptr = 0;
+    *ptr += 1;
+}
+}
+
+extern "C" {
+void throw_cpp()
+{
+    try {
+        // throws std::length_error
+        std::string("1").substr(2);
+    } catch (const std::exception &e) {
+        std::cout << e.what() << '\n';
+        throw;
+    }
+}
+}
