830224ab8c8d9f583befb4ff6e81c8e185f8754a
Ivan Dlugos <dlugos.ivan@gmail.com>
2022-07-20 21:31:22 +0200

    chore: forward slashes in integration-test.ps1

--- test/Scripts.Integration.Test/integration-test.ps1
+++ test/Scripts.Integration.Test/integration-test.ps1
@@ -2,8 +2,8 @@
     [string] $UnityPath
 )
 
-.\test/Scripts.Integration.Test/create-project.ps1 "$UnityPath"
-.\test/Scripts.Integration.Test/build-project.ps1 "$UnityPath"
-.\test/Scripts.Integration.Test/update-sentry.ps1 "$UnityPath"
-.\test/Scripts.Integration.Test/build-project.ps1 "$UnityPath"
-.\test/Scripts.Integration.Test/run-smoke-test.ps1 -Smoke
+./test/Scripts.Integration.Test/create-project.ps1 "$UnityPath"
+./test/Scripts.Integration.Test/build-project.ps1 "$UnityPath"
+./test/Scripts.Integration.Test/update-sentry.ps1 "$UnityPath"
+./test/Scripts.Integration.Test/build-project.ps1 "$UnityPath"
+./test/Scripts.Integration.Test/run-smoke-test.ps1 -Smoke
