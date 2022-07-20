8c5b4cc301e03e8e154cfd2d51600e3b6c784693
Ivan Dlugos <dlugos.ivan@gmail.com>
2022-07-20 21:40:58 +0200

    test: enable Il2CppLineNumberSupportEnabled in integration-test

--- test/Scripts.Integration.Test/update-sentry.ps1
+++ test/Scripts.Integration.Test/update-sentry.ps1
@@ -32,21 +32,22 @@ Copy-Item -Recurse "$IntegrationScriptsPath/Scripts/*" -Destination "$NewProject
 Write-Host " OK"
 
 $unityArgs = @( `
-    "-quit", "-batchmode", "-nographics", "-projectPath ", $NewProjectPath, `
-    "-executeMethod", "Sentry.Unity.Editor.ConfigurationWindow.SentryEditorWindowInstrumentation.ConfigureOptions", `
-    "-sentryOptions.Dsn", (TestDsnFor $Platform), `
-    "-sentryOptionsScript", "SmokeTestOptions", `
-    "-attachScreenshot", "true", `
-    "-diagnosticLevel", "debug")
+        "-quit", "-batchmode", "-nographics", "-projectPath ", $NewProjectPath, `
+        "-executeMethod", "Sentry.Unity.Editor.ConfigurationWindow.SentryEditorWindowInstrumentation.ConfigureOptions", `
+        "-sentryOptions.Dsn", (TestDsnFor $Platform), `
+        "-sentryOptionsScript", "SmokeTestOptions", `
+        "-attachScreenshot", "true", `
+        "-il2cppLineNumbers", "true", `
+        "-diagnosticLevel", "debug")
 
-if($CheckSymbols)
+if ($CheckSymbols)
 {
     $unityArgs += @( `
-        "-cliOptions.UploadSources", "true", `
-        "-cliOptions.Org", "sentry-sdks", `
-        "-cliOptions.Project", "sentry-unity", `
-        "-cliOptions.Auth", "dummy-token", `
-        "-cliOptions.UrlOverride", (SymbolServerUrlFor $UnityPath $Platform))
+            "-cliOptions.UploadSources", "true", `
+            "-cliOptions.Org", "sentry-sdks", `
+            "-cliOptions.Project", "sentry-unity", `
+            "-cliOptions.Auth", "dummy-token", `
+            "-cliOptions.UrlOverride", (SymbolServerUrlFor $UnityPath $Platform))
 }
 
 RunUnityAndExpect "ConfigureSentryOptions" "ConfigureOptions: Sentry options Configured" "ConfigureOptions failed" $unityArgs
