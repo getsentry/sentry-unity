6fe28b0fa37aacb655ef7f1f0cf579a8f3db348b
bitsandfoxes <reg@bitfox.at>
2022-08-10 15:43:44 +0200

    reflection for the plist manipulation

--- test/Scripts.Integration.Test/Editor/Builder.cs
+++ test/Scripts.Integration.Test/Editor/Builder.cs
@@ -1,10 +1,10 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Reflection;
 using UnityEditor;
 using UnityEditor.Build;
 using UnityEditor.Build.Reporting;
-using UnityEditor.iOS.Xcode;
 using UnityEngine;
 
 public class Builder
@@ -111,8 +111,6 @@ public class Builder
     }
 }
 
-// This started being necessary with Unity 2022 and older iOS versions (12.0 - 14.1)
-// Message: App Transport Security has blocked a cleartext HTTP (http://) resource load since it is insecure. Temporary exceptions can be configured via your app's Info.plist file.
 public class iOSCleartextHTTP : IPostprocessBuildWithReport
 {
     public int callbackOrder { get; }
@@ -122,20 +120,32 @@ public class iOSCleartextHTTP : IPostprocessBuildWithReport
         var pathToBuiltProject = report.summary.outputPath;
         if (report.summary.platform == BuildTarget.iOS)
         {
-            var plistPath = pathToBuiltProject + "/Info.plist";
-            var plist = new PlistDocument();
-            plist.ReadFromString(File.ReadAllText(plistPath));
+            var plistPath = Path.Combine(pathToBuiltProject, "Info.plist");
+            if (!File.Exists(plistPath))
+            {
+                Debug.LogError("Failed to find the plist.");
+                return;
+            }
 
-            var allowsDict = plist.root.CreateDict("NSAppTransportSecurity");
-            allowsDict.SetBoolean("NSAllowsArbitraryLoads", true);
+            var xcodeAssembly = Assembly.Load("UnityEditor.iOS.Extensions.Xcode");
+            var plistType = xcodeAssembly.GetType("UnityEditor.iOS.Xcode.PlistDocument");
+            var plistElementDictType = xcodeAssembly.GetType("UnityEditor.iOS.Xcode.PlistElementDict");
 
-            var exceptionsDict = allowsDict.CreateDict("NSExceptionDomains");
+            var plist = Activator.CreateInstance(plistType);
+            plistType.GetMethod("ReadFromString", BindingFlags.Public | BindingFlags.Instance)
+                ?.Invoke(plist, new object[] { File.ReadAllText(plistPath) });
 
-            var domainDict = exceptionsDict.CreateDict("localhost.exception");
-            domainDict.SetBoolean("NSExceptionAllowsInsecureHTTPLoads", true);
-            domainDict.SetBoolean("NSIncludesSubdomains", true);
+            var root = plistType.GetField("root", BindingFlags.Public | BindingFlags.Instance);
+            var allowDict = plistElementDictType.GetMethod("CreateDict", BindingFlags.Public | BindingFlags.Instance)
+                ?.Invoke(root?.GetValue(plist), new object[] { "NSAppTransportSecurity" });
 
-            File.WriteAllText(plistPath, plist.WriteToString());
+            plistElementDictType.GetMethod("SetBoolean", BindingFlags.Public | BindingFlags.Instance)
+                ?.Invoke(allowDict, new object[] { "NSAllowsArbitraryLoads", true });
+
+            var contents = (string)plistType.GetMethod("WriteToString", BindingFlags.Public | BindingFlags.Instance)
+                ?.Invoke(plist, null);
+
+            File.WriteAllText(plistPath, contents);
         }
     }
 }
