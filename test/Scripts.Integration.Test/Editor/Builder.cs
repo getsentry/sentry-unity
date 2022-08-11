14178be6b3b973beea9fa703060101a54a3e66c7
bitsandfoxes <reg@bitfox.at>
2022-08-11 11:10:30 +0200

    always allow insecure http for smoketesting

--- test/Scripts.Integration.Test/Editor/Builder.cs
+++ test/Scripts.Integration.Test/Editor/Builder.cs
@@ -17,6 +17,7 @@ public class Builder
         // Make sure the configuration is right.
         EditorUserBuildSettings.selectedBuildTargetGroup = group;
         PlayerSettings.SetScriptingBackend(group, ScriptingImplementation.IL2CPP);
+        PlayerSettings.insecureHttpOption = InsecureHttpOption.AlwaysAllowed;
         DisableUnityAudio();
 
         var buildPlayerOptions = new BuildPlayerOptions
