3777cb776a57df9a209993f45c11f3f1d27138c7
Ivan Dlugos <dlugos.ivan@gmail.com>
2022-09-23 16:41:54 +0200

    tmp

--- test.sh
+++ test.sh
@@ -1,3 +1,5 @@
 dotnet msbuild /t:UnityEditModeTest /p:Configuration=Release
 dotnet msbuild /t:UnityPlayModeTest /p:Configuration=Release
 dotnet msbuild /t:UnitySmokeTestStandalonePlayerIL2CPP /p:Configuration=Release
+
+// test
