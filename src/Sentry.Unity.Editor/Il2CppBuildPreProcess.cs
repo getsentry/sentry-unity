351cb00db8b45038ff6beac48657568c70bcc27a
Stefan Jandl <reg@bitfox.at>
2022-09-30 19:10:51 +0200

    fix: IL2CPP additional arguments (#997)
    * keeping the argument list clean
    
    * tests
    
    * Updated CHANGELOG.md
    
    * Update test/Sentry.Unity.Editor.Tests/Il2CppBuildPreProcess.cs

---
+++ src/Sentry.Unity.Editor/Il2CppBuildPreProcess.cs
@@ -0,0 +1,65 @@
+using System;
+using Sentry.Extensibility;
+using UnityEditor;
+using UnityEditor.Build;
+using UnityEditor.Build.Reporting;
+using UnityEngine;
+
+namespace Sentry.Unity.Editor
+{
+    internal class Il2CppBuildPreProcess : IPreprocessBuildWithReport
+    {
+        internal const string SourceMappingArgument = "--emit-source-mapping";
+
+        public int callbackOrder => 0;
+
+        public void OnPreprocessBuild(BuildReport report)
+        {
+            if (PlayerSettings.GetScriptingBackend(report.summary.platformGroup) != ScriptingImplementation.IL2CPP)
+            {
+                return;
+            }
+
+            var options = SentryScriptableObject
+                .Load<ScriptableSentryUnityOptions>(ScriptableSentryUnityOptions.GetConfigPath())
+                ?.ToSentryUnityOptions(BuildPipeline.isBuildingPlayer);
+
+            if (options is null)
+            {
+                return;
+            }
+
+            SetAdditionalIl2CppArguments(options,
+                PlayerSettings.GetAdditionalIl2CppArgs,
+                PlayerSettings.SetAdditionalIl2CppArgs);
+        }
+
+        internal static void SetAdditionalIl2CppArguments(SentryUnityOptions options, Func<string> getArguments, Action<string> setArguments)
+        {
+            if (options.Il2CppLineNumberSupportEnabled)
+            {
+                options.DiagnosticLogger?.LogDebug("IL2CPP line number support enabled - Adding additional IL2CPP arguments.");
+
+                var arguments = getArguments.Invoke();
+                if (arguments.Contains(SourceMappingArgument))
+                {
+                    options.DiagnosticLogger?.LogDebug("Additional argument '{0}' already present.", SourceMappingArgument);
+                    return;
+                }
+
+                setArguments.Invoke(getArguments.Invoke() + $" {SourceMappingArgument}");
+            }
+            else
+            {
+                var arguments = getArguments.Invoke();
+                if (arguments.Contains(SourceMappingArgument))
+                {
+                    options.DiagnosticLogger?.LogDebug("IL2CPP line number support disabled - Removing additional IL2CPP arguments.");
+
+                    arguments = arguments.Replace(SourceMappingArgument, "");
+                    setArguments.Invoke(arguments);
+                }
+            }
+        }
+    }
+}
---
+++ test/Sentry.Unity.Editor.Tests/Il2CppBuildPreProcess.cs
@@ -0,0 +1,87 @@
+using System;
+using System.Linq;
+using NUnit.Framework;
+
+namespace Sentry.Unity.Editor.Tests
+{
+    public class Il2CppBuildPreProcessTests
+    {
+        private string arguments = null!;
+        private string resultingArguments = null!;
+
+        [SetUp]
+        public void Setup()
+        {
+            arguments = string.Empty;
+            resultingArguments = string.Empty;
+        }
+
+        [Test]
+        public void SetAdditionalArguments_Il2CppEnabled_AddsArguments()
+        {
+            var options = new SentryUnityOptions { Il2CppLineNumberSupportEnabled = true };
+
+            Il2CppBuildPreProcess.SetAdditionalIl2CppArguments(options, () => arguments, s => resultingArguments = s);
+
+            Assert.That(resultingArguments, Does.Contain(Il2CppBuildPreProcess.SourceMappingArgument));
+        }
+
+        [Test]
+        public void SetAdditionalArguments_Il2CppDisabled_FoesNotAddArguments()
+        {
+            var options = new SentryUnityOptions { Il2CppLineNumberSupportEnabled = false };
+
+            Il2CppBuildPreProcess.SetAdditionalIl2CppArguments(options, () => arguments, s => resultingArguments = s);
+
+            Assert.That(resultingArguments, Does.Not.Contain(Il2CppBuildPreProcess.SourceMappingArgument));
+        }
+
+        [Test]
+        public void SetAdditionalArguments_Il2CppEnabled_ExistingArgumentsDoNotGetOverwritten()
+        {
+            var options = new SentryUnityOptions { Il2CppLineNumberSupportEnabled = true };
+            var expectedArgument = "--MyArgument";
+            arguments = expectedArgument;
+
+            Il2CppBuildPreProcess.SetAdditionalIl2CppArguments(options, () => arguments, s => resultingArguments = s);
+
+            Assert.That(resultingArguments, Does.Contain(Il2CppBuildPreProcess.SourceMappingArgument)); // sanity check
+            Assert.That(resultingArguments, Does.Contain(expectedArgument));
+        }
+
+        [Test]
+        public void SetAdditionalArguments_Il2CppEnabledAndArgumentAlreadyAdded_AddsArgumentsOnlyOnce()
+        {
+            var options = new SentryUnityOptions { Il2CppLineNumberSupportEnabled = true };
+
+            Il2CppBuildPreProcess.SetAdditionalIl2CppArguments(options, () => arguments, s => resultingArguments = s);
+            Assert.That(resultingArguments, Does.Contain(Il2CppBuildPreProcess.SourceMappingArgument)); // sanity check
+
+            Il2CppBuildPreProcess.SetAdditionalIl2CppArguments(options, () => arguments, s => resultingArguments = s);
+
+            Assert.That(resultingArguments, Does.Contain(Il2CppBuildPreProcess.SourceMappingArgument)); // sanity check
+            var occurrences = 0;
+            var i = 0;
+            while ((i = resultingArguments.IndexOf(Il2CppBuildPreProcess.SourceMappingArgument, i, StringComparison.Ordinal)) != -1)
+            {
+                i += Il2CppBuildPreProcess.SourceMappingArgument.Length;
+                occurrences++;
+            }
+
+            Assert.AreEqual(1, occurrences);
+        }
+
+        [Test]
+        public void SetAdditionalArguments_Il2CppDisabledAndArgumentAlreadyAdded_RemovesArguments()
+        {
+            var options = new SentryUnityOptions { Il2CppLineNumberSupportEnabled = false };
+            var expectedArgument = "--MyArgument";
+            arguments = $"{expectedArgument} {Il2CppBuildPreProcess.SourceMappingArgument}";
+
+            Il2CppBuildPreProcess.SetAdditionalIl2CppArguments(options, () => arguments, s => resultingArguments = s);
+
+            Assert.That(resultingArguments, Does.Contain(expectedArgument));
+            Assert.That(resultingArguments, Does.Not.Contain(Il2CppBuildPreProcess.SourceMappingArgument));
+        }
+    }
+}
