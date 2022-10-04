c293b8fb188e14aa4b1febc18df359bbd848582c
bitsandfoxes <reg@bitfox.at>
2022-10-04 11:01:53 +0200

    tweaking template scripts

--- samples/unity-of-bugs/Assets/Scripts/SentryAutoBehaviour.cs
+++ samples/unity-of-bugs/Assets/Scripts/SentryAutoBehaviour.cs
@@ -8,7 +8,28 @@ public class SentryAutoBehaviour : MonoBehaviour
         // SentrySdk.GetSpan()?.StartChild("Awake", $"{gameObject.name}.{name}");
         SentryAwakeIntegration.StartSpan(this);
 
-        Debug.Log("HUEHUE");
+        Debug.Log("Testing Awake");
+        
+        if (enabled)
+        {
+            Debug.Log("HUEHUE");
+
+            SentryAwakeIntegration.FinishSpan();
+            return;
+        }
+        else if (Time.deltaTime > 2.0f)
+        {
+            Debug.Log("sad noises");
+
+            SentryAwakeIntegration.FinishSpan();
+            return;
+        }
+        else
+        {
+            Debug.Log("End of if");
+        }
+
+        Debug.Log("End of testing Awake");
 
         SentryAwakeIntegration.FinishSpan();
         // SentrySdk.GetSpan()?.Finish(SpanStatus.Ok);
--- samples/unity-of-bugs/Assets/Scripts/SentryEmptyBehaviour.cs
+++ samples/unity-of-bugs/Assets/Scripts/SentryEmptyBehaviour.cs
@@ -1,11 +1,27 @@
-using System;
 using UnityEngine;
 
 public class SentryEmptyBehaviour : MonoBehaviour
 {
     private void Awake()
     {
-        Debug.Log("HUEHUE");
+        Debug.Log("Testing Awake");
+
+        if (enabled)
+        {
+            Debug.Log("HUEHUE");
+            return;
+        }
+        else if (Time.deltaTime > 2.0f)
+        {
+            Debug.Log("sad noises");
+            return;
+        }
+        else
+        {
+            Debug.Log("End of if");
+        }
+
+        Debug.Log("End of testing Awake");
     }
 
     private void Start()
