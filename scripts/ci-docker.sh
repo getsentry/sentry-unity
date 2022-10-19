f9d046a02190826f1c39197a717b978716197f12
Ivan Dlugos <dlugos.ivan@gmail.com>
2022-10-19 15:29:18 +0200

    ci: fixup integration test scripts

--- scripts/ci-docker.sh
+++ scripts/ci-docker.sh
@@ -1,20 +1,21 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-# unityVersion=$(pwsh ./scripts/ci-env.ps1 "unity$1")
-unityVersion="2019.4.40f1"
+unityPrefix="$1"
+unityVersion=$(pwsh ./scripts/ci-env.ps1 "unity$unityPrefix")
 imageVariant=$(echo "$2" | tr '[:upper:]' '[:lower:]')
 licenseConfig=$3
 
-# 1. We use the host dotnet installation - it's much faster than installing inside the docker container.
-# 2. We must use the iOS version of the image instead of 'base' - Sentry.Unity.Editor.iOS.csproj requires some libraries.
-#    Maybe we could just cache the needed file instead of pulling the 1 GB larger image on every build...
-
 container="unity"
 image="unityci/editor:ubuntu-$unityVersion-$imageVariant-1.0.1"
+cwd="${GITHUB_WORKSPACE:-$(pwd)}"
 user="gh"
 uid=$(id -u)
 gid=$(id -g)
+
+# Local fallbacks for testing
+ANDROID_HOME=${ANDROID_HOME:-/android-home-missing}
+JAVA_HOME_11_X64=${JAVA_HOME_11_X64:-/java-home-missing}
 
 if [[ $(docker ps --filter "name=^/$container$" --format '{{.Names}}') == "$container" ]]; then
     echo "Removing existing container '$container'"
@@ -23,18 +24,18 @@ if [[ $(docker ps --filter "name=^/$container$" --format '{{.Names}}') == "$cont
 fi
 
 echo "Starting up '$image' as '$container'"
+suexec="docker exec --user root"
 
+# We use the host dotnet installation - it's much faster than installing inside the docker container.
+set -x
 docker run -td --name $container \
     --user $uid:$gid \
-    -v $GITHUB_WORKSPACE:/sentry-unity \
+    -v "$cwd":/sentry-unity \
+    -v $ANDROID_HOME:$ANDROID_HOME \
+    -v $JAVA_HOME_11_X64:$JAVA_HOME_11_X64 \
     -v /usr/share/dotnet:/usr/share/dotnet \
     -v /opt/microsoft/powershell/7:/opt/microsoft/powershell/7 \
     --workdir /sentry-unity $image
-
-# -v $ANDROID_HOME:$ANDROID_HOME \
-# -v $JAVA_HOME_11_X64:$JAVA_HOME_11_X64 \
-
-suexec="docker exec --user root"
 
 $suexec $container groupadd -g $gid $user
 $suexec $container useradd -u $uid -g $gid --create-home $user
@@ -44,7 +45,12 @@ $suexec $container ln -s /opt/microsoft/powershell/7/pwsh /usr/bin/pwsh
 
 $suexec $container mkdir -p /usr/share/unity3d/config/
 echo $licenseConfig | $suexec -i $container sh -c "cat > /usr/share/unity3d/config/services-config.json"
-$suexec $container chown -R $(id -u) /usr/share/unity3d/config/
+$suexec $container chown -R $uid /usr/share/unity3d/config/
+
+# Unity 2021+ tries to write to this directory during asset import...
+if [[ $unityPrefix -ge 2021 ]]; then
+    $suexec $container chmod -R 755 /opt/unity/Editor/Data/UnityReferenceAssemblies/
+fi
 
 echo "Container started successfully: "
 docker ps --filter "name=^/$container$"
