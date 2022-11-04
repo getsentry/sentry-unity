bf3849beee5b23def4a979ca3afea8cdaf8e5b4b
Ivan Dlugos <dlugos.ivan@gmail.com>
2022-11-04 18:47:23 +0100

    fix: sample CustomOptionsConfiguration missing asset

--- samples/unity-of-bugs/Assets/Scripts/CustomOptionsConfiguration.cs
+++ samples/unity-of-bugs/Assets/Scripts/CustomOptionsConfiguration.cs
@@ -2,7 +2,7 @@ using Sentry.Unity;
 using UnityEngine;
 
 /// Learn more at https://docs.sentry.io/platforms/unity/configuration/options/#programmatic-configuration
-[CreateAssetMenu(fileName = "Assets/Resources/Sentry/CustomOptionsConfiguration", menuName = "Sentry/CustomOptionsConfiguration", order = 999)]
+[CreateAssetMenu(fileName = "Assets/Resources/Sentry/CustomOptionsConfiguration.cs", menuName = "Sentry/CustomOptionsConfiguration", order = 999)]
 public class CustomOptionsConfiguration : ScriptableOptionsConfiguration
 {
     /// See base class for documentation.
