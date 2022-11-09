7d35b052392f56dab55a1855fbf57444bb456c9a
bitsandfoxes <reg@bitfox.at>
2022-11-09 15:03:39 +0100

    using cleanup

--- src/Sentry.Unity/UnityEventProcessor.cs
+++ src/Sentry.Unity/UnityEventProcessor.cs
@@ -1,7 +1,6 @@
 using System;
 using Sentry.Extensibility;
 using Sentry.Protocol;
-using Sentry.Unity.Integrations;
 using UnityEngine;
 using DeviceOrientation = Sentry.Protocol.DeviceOrientation;
 
