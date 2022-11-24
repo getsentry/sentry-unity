6755d10b60e6af2048453aa0c0cf4474e234c00c
bitsandfoxes <reg@bitfox.at>
2022-11-24 13:32:42 +0100

    fixed 0 check

--- src/Sentry.Unity.Editor.iOS/SentryXcodeProject.cs
+++ src/Sentry.Unity.Editor.iOS/SentryXcodeProject.cs
@@ -25,7 +25,7 @@ namespace Sentry.Unity.Editor.iOS
 
 process_upload_symbols() {{
     ./{0} --log-level=debug upload-dif {1} $BUILT_PRODUCTS_DIR &> sentry-symbols-upload.log &
-    if [ $? -eq 0] ; then
+    if [ $? -eq ""0"" ] ; then
         echo ""note: Debug symbol upload successful.""
     else
         echo ""error: Debug symbol upload failed. See './sentry-symbols-upload.log' for more information.""
