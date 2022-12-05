345e3ede0f1db88b69208a55dac57d99be725f7e
bitsandfoxes <reg@bitfox.at>
2022-12-05 15:48:50 +0100

    split init

--- src/Sentry.Unity/SentryUnity.cs
+++ src/Sentry.Unity/SentryUnity.cs
@@ -15,10 +15,7 @@ namespace Sentry.Unity
     /// </summary>
     public static class SentryUnity
     {
-        private static IDisposable? DotnetSdk;
-        private static SentryUnityOptions? Options;
-
-        private static FileStream? LockFile;
+        private static SentryUnitySDK? UnitySdk;
 
         /// <summary>
         /// Initializes Sentry Unity SDK while configuring the options.
@@ -28,6 +25,7 @@ namespace Sentry.Unity
         {
             var options = new SentryUnityOptions();
             sentryUnityOptionsConfigure.Invoke(options);
+
             Init(options);
         }
 
@@ -38,56 +36,7 @@ namespace Sentry.Unity
         [EditorBrowsable(EditorBrowsableState.Never)]
         public static void Init(SentryUnityOptions options)
         {
-            Options = options;
-
-            Options.SetupLogging();
-            if (Options.ShouldInitializeSdk())
-            {
-                // On Standalone, we disable cache dir in case multiple app instances run over the same path.
-                // Note: we cannot use a named Mutex, because Unit doesn't support it. Instead, we create a file with `FileShare.None`.
-                // https://forum.unity.com/threads/unsupported-internal-call-for-il2cpp-mutex-createmutex_internal-named-mutexes-are-not-supported.387334/
-                if (ApplicationAdapter.Instance.Platform is RuntimePlatform.WindowsPlayer && Options.CacheDirectoryPath is not null)
-                {
-                    try
-                    {
-                        LockFile = new FileStream(Path.Combine(Options.CacheDirectoryPath, "sentry-unity.lock"), FileMode.OpenOrCreate,
-                                FileAccess.ReadWrite, FileShare.None);
-                    }
-                    catch (Exception ex)
-                    {
-                        Options.DiagnosticLogger?.LogWarning("An exception was thrown while trying to " +
-                                                             "acquire a lockfile on the config directory: .NET event cache will be disabled.", ex);
-                        Options.CacheDirectoryPath = null;
-                        Options.AutoSessionTracking = false;
-                    }
-                }
-
-                DotnetSdk = SentrySdk.Init(options);
-
-                if (Options.AttachScreenshot)
-                {
-                    SentrySdk.ConfigureScope(s =>
-                        s.AddAttachment(new ScreenshotAttachment(
-                            new ScreenshotAttachmentContent(options, SentryMonoBehaviour.Instance))));
-                }
-
-                if (Options.NativeContextWriter is { } contextWriter)
-                {
-                    SentrySdk.ConfigureScope((scope) =>
-                    {
-                        var task = Task.Run(() => contextWriter.Write(scope)).ContinueWith(t =>
-                        {
-                            if (t.Exception is not null)
-                            {
-                                Options.DiagnosticLogger?.LogWarning(
-                                    "Failed to synchronize scope to the native SDK: {0}", t.Exception);
-                            }
-                        });
-                    });
-                }
-
-                ApplicationAdapter.Instance.Quitting += Close;
-            }
+            UnitySdk = SentryUnitySDK.Init(options);
         }
 
         /// <summary>
@@ -96,36 +45,8 @@ namespace Sentry.Unity
         [EditorBrowsable(EditorBrowsableState.Never)]
         public static void Close()
         {
-            Options?.DiagnosticLogger?.LogDebug("Closing the sentry-dotnet SDK");
-            try
-            {
-                ApplicationAdapter.Instance.Quitting -= Close;
-
-                if (Options is not null)
-                {
-                    Options.NativeSupportCloseCallback?.Invoke();
-                    Options.NativeSupportCloseCallback = null;
-                }
-
-                DotnetSdk?.Dispose();
-                DotnetSdk = null;
-            }
-            catch (Exception ex)
-            {
-                Options?.DiagnosticLogger?.Log(SentryLevel.Warning,
-                    "Exception while closing the .NET SDK.", ex);
-            }
-
-            try
-            {
-                // We don't really need to close, Windows would release the lock anyway, but let's be nice.
-                LockFile?.Close();
-            }
-            catch (Exception ex)
-            {
-                Options?.DiagnosticLogger?.Log(SentryLevel.Warning,
-                    "Exception while releasing the lockfile on the config directory.", ex);
-            }
+            UnitySdk?.Close();
+            UnitySdk = null;
         }
     }
 }
