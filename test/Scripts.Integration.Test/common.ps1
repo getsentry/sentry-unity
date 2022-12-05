9aad68307bea08606f3363248257e4458aa36f8e
bitsandfoxes <reg@bitfox.at>
2022-12-05 16:04:13 +0100

    fixed 2020 expectations

--- test/Scripts.Integration.Test/common.ps1
+++ test/Scripts.Integration.Test/common.ps1
@@ -289,8 +289,8 @@ function CheckSymbolServerOutput([string] $buildMethod, [string] $symbolServerOu
             If ($unity2020OrHigher)
             {
                 $expectedFiles = @(
-                    "libil2cpp.sym.so: count=$($withSources ? 2 : 1)",
-                    "libil2cpp.dbg.so: count=$($withSources ? 3 : 2)"
+                    "libil2cpp.sym.so: count=$($withSources ? 3 : 2)",
+                    "libil2cpp.dbg.so: count=$($withSources ? 2 : 1)"
                 ) + $expectedFiles
             }
             else
