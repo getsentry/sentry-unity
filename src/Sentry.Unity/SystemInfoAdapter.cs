11363aa560d797dff10a6b43526dbc37f50cf7e5
bitsandfoxes <reg@bitfox.at>
2023-06-09 12:32:53 +0200

    check for version

--- src/Sentry.Unity/SystemInfoAdapter.cs
+++ src/Sentry.Unity/SystemInfoAdapter.cs
@@ -56,7 +56,24 @@ namespace Sentry.Unity
         public bool? SupportsVibration => SystemInfo.supportsVibration;
         public Lazy<string>? DeviceType => new(() => SystemInfo.deviceType.ToString());
         public string? CpuDescription => SystemInfo.processorType;
-        public string? DeviceName => SystemInfo.deviceName;
+        public string? DeviceName
+        {
+            get
+            {
+                // Workaround for https://github.com/getsentry/sentry-unity/issues/1322 -
+                if (Application.platform == RuntimePlatform.Android)
+                {
+                    using var version = new AndroidJavaClass("android.os.Build$VERSION");
+                    var sdkVersion = version.GetStatic<int>("SDK_INT");
+                    if (sdkVersion >= 32)
+                    {
+                        // Unity's built-in fallback
+                        return "<unknown>";
+                    }
+                }
+                return SystemInfo.deviceName;
+            }
+        }
 
         public Lazy<string> DeviceUniqueIdentifier => new(() => SystemInfo.deviceUniqueIdentifier);
         public Lazy<string> DeviceModel => new(() => SystemInfo.deviceModel);
