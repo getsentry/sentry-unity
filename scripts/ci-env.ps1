d49d9a2b216416da7c442ba50149d2b4ad0d28ea
vaind <vaind@users.noreply.github.com>
2023-10-24 03:00:53 +0000

    chore: update to Unity 2022.3.11f1

--- scripts/ci-env.ps1
+++ scripts/ci-env.ps1
@@ -14,7 +14,7 @@ switch ($name) {
         return "2021.3.31f1"
     }
     "unity2022" {
-        return "2022.1.23f1"
+        return "2022.3.11f1"
     }
     Default {
         throw "Unkown variable '$name'"
