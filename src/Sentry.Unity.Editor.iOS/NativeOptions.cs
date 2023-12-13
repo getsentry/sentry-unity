468b2535534198d86dfc1a61a1c177d13fe2afd9
bitsandfoxes <reg@bitfox.at>
2023-12-13 16:29:24 +0100

    undo

--- src/Sentry.Unity.Editor.iOS/NativeOptions.cs
+++ src/Sentry.Unity.Editor.iOS/NativeOptions.cs
@@ -30,7 +30,7 @@ static SentryOptions* getSentryOptions()
         @""enableAutoSessionTracking"": @NO,
         @""enableAppHangTracking"": @NO,
         @""sendDefaultPii"" : @{ToObjCString(options.SendDefaultPii)},
-        @""attachScreenshot"" : @{ToObjCString(options.AttachScreenshot)},
+        @""attachScreenshot"" : @""{{options.AttachScreenshot}}"",
         @""release"" : @""{options.Release}"",
         @""environment"" : @""{options.Environment}""
     }};
