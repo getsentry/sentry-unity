a93414dfd0f4adbee2696fe9f070fe63cbcb8e70
bitsandfoxes <reg@bitfox.at>
2024-01-08 10:10:25 +0100

    ignore right before?

--- test/Sentry.Unity.Tests/TestBehaviours/TestMonoBehaviour.cs
+++ test/Sentry.Unity.Tests/TestBehaviours/TestMonoBehaviour.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Threading.Tasks;
 using UnityEngine;
+using UnityEngine.TestTools;
 
 namespace Sentry.Unity.Tests.TestBehaviours
 {
@@ -12,7 +13,13 @@ namespace Sentry.Unity.Tests.TestBehaviours
         public void ThrowException(string message) => throw new Exception(message);
         public void DebugLogError(string message) => Debug.LogError(message);
         public void DebugLogErrorInTask(string message) => Task.Run(() => DebugLogError(message));
-        public void DebugLogException(string message) => Debug.LogException(new Exception(message));
+        public void DebugLogException(string message)
+        {
+            // Don't fail test if an exception is thrown via 'SendMessage'. We want to continue.
+            LogAssert.ignoreFailingMessages = true;
+            Debug.LogException(new Exception(message));
+        }
+
         public void DebugLogExceptionInTask(string message) => Task.Run(() => DebugLogException(message));
     }
 }
