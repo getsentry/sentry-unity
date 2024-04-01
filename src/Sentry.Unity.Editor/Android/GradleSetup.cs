f5e9a7b665ccc2de33398c7f06993cebee4fbea6
Andriy Marshalek <amarshalek@ninevastudios.com>
2024-04-01 11:13:19 +0300

    replace only the first occurrence of the marker in the gradle file (#1477)

--- src/Sentry.Unity.Editor/Android/GradleSetup.cs
+++ src/Sentry.Unity.Editor/Android/GradleSetup.cs
@@ -1,4 +1,5 @@
 using System.IO;
+using System.Text.RegularExpressions;
 using Sentry.Extensibility;
 
 namespace Sentry.Unity.Editor.Android
@@ -66,7 +67,8 @@ namespace Sentry.Unity.Editor.Android
                 return fileContent;
             }
 
-            return fileContent.Replace(textToFind, textToInsert);
+            var regex = new Regex(Regex.Escape(textToFind));
+            return regex.Replace(fileContent, textToInsert, 1);
         }
 
         internal static string LoadGradleScript(string path)
