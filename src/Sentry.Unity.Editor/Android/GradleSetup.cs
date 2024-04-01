33430a91fe9e7ad216e36831dcb5cfa23182094d
Andriy Marshalek <amarshalek@ninevastudios.com>
2024-04-01 14:24:47 +0300

    rely only on android tag in the build gradle (#1477)

--- src/Sentry.Unity.Editor/Android/GradleSetup.cs
+++ src/Sentry.Unity.Editor/Android/GradleSetup.cs
@@ -9,11 +9,10 @@ namespace Sentry.Unity.Editor.Android
         private readonly IDiagnosticLogger _logger;
 
         private const string AndroidMarker = "android {";
-        private const string DependenciesMarker = "dependencies {";
         private const string SdkDependenciesFull = SdkDependencies + "\n}\n\n" + AndroidMarker;
 
-        public const string SdkDependencies = DependenciesMarker + "\n\timplementation(name: 'sentry-android-ndk-release', ext:'aar')\n\timplementation(name: 'sentry-android-core-release', ext:'aar')";
-
+        public const string SdkDependencies = "dependencies {\n\timplementation(name: 'sentry-android-ndk-release', ext:'aar')\n\timplementation(name: 'sentry-android-core-release', ext:'aar')";
+        public const string DependenciesAddedMessage = "The Sentry Gradle dependencies have already been added.";
         private readonly string _unityLibraryGradle;
 
         public GradleSetup(IDiagnosticLogger logger, string gradleProjectPath)
@@ -26,7 +25,7 @@ namespace Sentry.Unity.Editor.Android
         {
             _logger.LogDebug("Adding Sentry to the gradle project.");
             var fileContent = LoadGradleScript(_unityLibraryGradle);
-            fileContent = ReplaceGradleContents(fileContent);
+            fileContent = AddSentryToGradle(fileContent);
             File.WriteAllText(_unityLibraryGradle, fileContent);
         }
 
@@ -34,41 +33,27 @@ namespace Sentry.Unity.Editor.Android
         {
             _logger.LogDebug("Removing Sentry from the gradle project.");
             var fileContent = LoadGradleScript(_unityLibraryGradle);
-            var hasFullDependencies = fileContent.Contains(SdkDependenciesFull);
-            var textToFind = hasFullDependencies ? SdkDependenciesFull : SdkDependencies;
-            var textToInsert = hasFullDependencies ? AndroidMarker : DependenciesMarker;
 
-            if (!fileContent.Contains(textToFind))
+            if (!fileContent.Contains(SdkDependenciesFull))
             {
                 _logger.LogDebug("The Sentry Gradle dependencies have already been removed.");
                 return;
             }
 
-            fileContent = fileContent.Replace(textToFind, textToInsert);
+            fileContent = fileContent.Replace(SdkDependenciesFull, AndroidMarker);
             File.WriteAllText(_unityLibraryGradle, fileContent);
         }
 
-        public string ReplaceGradleContents(string fileContent)
+        public string AddSentryToGradle(string fileContent)
         {
-            var hasDependenciesSection = fileContent.Contains(DependenciesMarker);
-            var textToInsert = hasDependenciesSection ? SdkDependencies : SdkDependenciesFull;
-            var textToFind = hasDependenciesSection ? DependenciesMarker : AndroidMarker;
-
-            if (fileContent.Contains(textToInsert))
+            if (fileContent.Contains(SdkDependenciesFull))
             {
-                _logger.LogDebug("The Sentry Gradle dependencies have already been added.");
+                _logger.LogDebug(DependenciesAddedMessage);
                 return fileContent;
             }
 
-            if (!fileContent.Contains(textToFind))
-            {
-                _logger.LogError("Could not find marker: " + textToFind +
-                                 "to add Sentry Gradle dependencies. Please, make sure your mainGradle.template contains either dependencies or android section.");
-                return fileContent;
-            }
-
-            var regex = new Regex(Regex.Escape(textToFind));
-            return regex.Replace(fileContent, textToInsert, 1);
+            var regex = new Regex(Regex.Escape(AndroidMarker));
+            return regex.Replace(fileContent, SdkDependenciesFull, 1);
         }
 
         internal static string LoadGradleScript(string path)
