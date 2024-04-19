6013707855076c23e4f795dca9255125e225db3e
Andriy Marshalek <amarshalek@ninevastudios.com>
2024-04-19 15:39:44 +0300

    create output build directory for android integration tests during test run

--- test/Scripts.Integration.Test/Editor/Builder.cs
+++ test/Scripts.Integration.Test/Editor/Builder.cs
@@ -42,6 +42,7 @@ public class Builder
 #endif
         if (target == BuildTarget.Android)
         {
+            Directory.CreateDirectory("Build");
 #if UNITY_2020_1_OR_NEWER
             PlayerSettings.Android.minifyDebug = PlayerSettings.Android.minifyRelease = true;
             PlayerSettings.Android.minifyWithR8 = true;
