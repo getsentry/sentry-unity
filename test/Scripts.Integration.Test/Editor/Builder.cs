9311a93c6d78efa1b54f0e3412ca49ec17a6cd64
Andriy Marshalek <amarshalek@ninevastudios.com>
2024-04-19 13:56:43 +0300

    enable android minify for CI builds

--- test/Scripts.Integration.Test/Editor/Builder.cs
+++ test/Scripts.Integration.Test/Editor/Builder.cs
@@ -40,6 +40,16 @@ public class Builder
             PlayerSettings.SetIl2CppCompilerConfiguration(group, Il2CppCompilerConfiguration.Debug);
         }
 #endif
+        if (target == BuildTarget.Android)
+        {
+#if UNITY_2020_1_OR_NEWER
+            PlayerSettings.Android.minifyDebug = PlayerSettings.Android.minifyRelease = true;
+            PlayerSettings.Android.minifyWithR8 = true;
+#else
+            EditorUserBuildSettings.androidDebugMinification =
+                EditorUserBuildSettings.androidReleaseMinification = AndroidMinification.Proguard;
+#endif
+        }
 
         var buildPlayerOptions = new BuildPlayerOptions
         {
