2e0df882a29649e8626b40b1e5204a33cd96d129
Andriy Marshalek <41358286+munkiki7@users.noreply.github.com>
2024-05-23 12:29:21 +0300

    change gradle task syntax and placement (#1657)

--- test/Sentry.Unity.Editor.Tests/Android/AndroidManifestConfigurationTests.cs
+++ test/Sentry.Unity.Editor.Tests/Android/AndroidManifestConfigurationTests.cs
@@ -353,8 +353,8 @@ namespace Sentry.Unity.Editor.Tests.Android
             sut.SetupSymbolsUpload(dsuFixture.UnityProjectPath, dsuFixture.GradleProjectPath);
 
             StringAssert.Contains("println 'Uploading symbols to Sentry",
-                File.ReadAllText(Path.Combine(dsuFixture.GradleProjectPath, "build.gradle")));
-            Assert.True(File.Exists(Path.Combine(dsuFixture.GradleProjectPath, "sentry.properties")));
+                File.ReadAllText(Path.Combine(dsuFixture.GradleProjectPath, "launcher/build.gradle")));
+            Assert.True(File.Exists(Path.Combine(dsuFixture.GradleProjectPath, "launcher/sentry.properties")));
 
             Directory.Delete(Path.GetFullPath(dsuFixture.FakeProjectPath), true);
         }
