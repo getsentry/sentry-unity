cbe11d219635371f489117554936bb62f25243d3
bitsandfoxes <reg@bitfox.at>
2024-06-05 20:19:01 +0200

    order is important

--- test/Scripts.Integration.Test/configure-sentry.ps1
+++ test/Scripts.Integration.Test/configure-sentry.ps1
@@ -10,7 +10,7 @@ param(
 $UnityPath = FormatUnityPath $UnityPath
 
 $unityArgs = @( `
-        "-quit", "-batchmode", "-nographics", "-projectPath -disable-assembly-updater ", $NewProjectPath, `
+        "-quit", "-batchmode", "-nographics", "-disable-assembly-updater", "-projectPath ", $NewProjectPath, `
         "-executeMethod", "Sentry.Unity.Editor.ConfigurationWindow.SentryEditorWindowInstrumentation.ConfigureOptions", `
         "-buildTimeOptionsScript", "BuildTimeOptions", `
         "-runtimeOptionsScript", "RuntimeOptions", `
