61f9ee61b261ace472c70d1bc05ee6fa1b21c0ba
Stefan Jandl <reg@bitfox.at>
2024-06-05 22:37:41 +0200

    Fix/disable asm updatr (#1681)

--- test/Scripts.Integration.Test/configure-sentry.ps1
+++ test/Scripts.Integration.Test/configure-sentry.ps1
@@ -10,7 +10,7 @@ param(
 $UnityPath = FormatUnityPath $UnityPath
 
 $unityArgs = @( `
-        "-quit", "-batchmode", "-nographics", "-projectPath ", $NewProjectPath, `
+        "-quit", "-batchmode", "-nographics", "-disable-assembly-updater", "-projectPath ", $NewProjectPath, `
         "-executeMethod", "Sentry.Unity.Editor.ConfigurationWindow.SentryEditorWindowInstrumentation.ConfigureOptions", `
         "-buildTimeOptionsScript", "BuildTimeOptions", `
         "-runtimeOptionsScript", "RuntimeOptions", `
