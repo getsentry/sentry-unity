22d4623a77a15db336811e9cbc94a134ae895bc7
vaind <vaind@users.noreply.github.com>
2024-07-23 03:01:04 +0000

    chore: update to Unity 2021.3.40f1

--- scripts/ci-env.ps1
+++ scripts/ci-env.ps1
@@ -11,7 +11,7 @@ switch ($name) {
         return "2020.3.48f1"
     }
     "unity2021" {
-        return "2021.3.38f1"
+        return "2021.3.40f1"
     }
     "unity2022" {
         return "2022.3.37f1"
