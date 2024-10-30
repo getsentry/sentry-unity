4df953801eaf87d6de850c9756942dbf51637c4f
Stefan Jandl <reg@bitfox.at>
2024-10-30 11:54:20 +0100

    disable flakey test (#1869)

---
+++ test/SharedClasses/TestEnvironment.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace Sentry.Unity.Tests.SharedClasses;
+
+public static class TestEnvironment
+{
+    /// <summary>
+    /// See https://docs.github.com/en/actions/writing-workflows/choosing-what-your-workflow-does/store-information-in-variables#default-environment-variables
+    /// </summary>
+    public static bool IsGitHubActions
+    {
+        get
+        {
+            var isGitHubActions = Environment.GetEnvironmentVariable("GITHUB_ACTIONS");
+            return isGitHubActions?.Equals("true", StringComparison.OrdinalIgnoreCase) == true;
+        }
+    }
+}
