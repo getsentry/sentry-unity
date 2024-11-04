2fc1f3a6b88ea196519ae39d8950f6f6c7eb0c34
bitsandfoxes <reg@bitfox.at>
2024-11-04 19:28:32 +0100

    upsi

--- scripts/tmp.ps1
+++
@@ -1,4 +0,0 @@
-Write-Host "Retrieving list of available simulators" -ForegroundColor Green
-$deviceListRaw = xcrun simctl list devices
-Write-Host "Available simulators:" -ForegroundColor Green
-$deviceListRaw | Write-Host
