45852f86e95adc0d63a1986156099a2d0a5cdf63
bitsandfoxes <reg@bitfox.at>
2024-11-18 12:14:02 +0100

    bug-config

--- samples/unity-of-bugs/Assets/Scripts/SentryOptionConfiguration.cs
+++ samples/unity-of-bugs/Assets/Scripts/SentryOptionConfiguration.cs
@@ -1,6 +1,7 @@
 using Sentry;
 using Sentry.Unity;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SentryOptionConfiguration : SentryOptionsConfiguration
 {
@@ -19,6 +20,30 @@ public class SentryOptionConfiguration : SentryOptionsConfiguration
 
         // Will NOT work as expected as this will need to get validated at runtime
         // options.Debug = SystemInfo.deviceName.Contains("Pixel");
+
+        Debug.Log(nameof(RuntimeConfiguration) + "::Configure() called");
+
+        // Making sure the SDK is not already initialized during tests
+        var sceneName = SceneManager.GetActiveScene().name;
+        if (sceneName != null && sceneName.Contains("TestScene"))
+        {
+            Debug.Log("Disabling the SDK while running tests.");
+            options.Enabled = false;
+        }
+
+        // BeforeSend is only relevant at runtime. It wouldn't hurt to be set at build time, just wouldn't do anything.
+        options.SetBeforeSend((sentryEvent, _) =>
+        {
+            if (sentryEvent.Tags.ContainsKey("SomeTag"))
+            {
+                // Don't send events with a tag SomeTag to Sentry
+                return null;
+            }
+
+            return sentryEvent;
+        });
+
+        Debug.Log(nameof(RuntimeConfiguration) + "::Configure() finished");
 #endif
     }
 }
