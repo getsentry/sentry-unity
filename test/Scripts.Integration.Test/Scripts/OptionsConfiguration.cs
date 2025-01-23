4d4e2ddcb49889f93d5b5c36aadf2c06292c4a3c
Stefan Jandl <reg@bitfox.at>
2025-01-23 17:14:56 +0100

    feat: CI runs for `runtime` and `buildtime` init on mobile (#1944)

--- test/Scripts.Integration.Test/Scripts/OptionsConfiguration.cs
+++ test/Scripts.Integration.Test/Scripts/OptionsConfiguration.cs
@@ -40,6 +40,10 @@ public class OptionsConfiguration : SentryOptionsConfiguration
         // If an ANR triggers while the smoke test runs, the test would fail because we expect exact order of events.
         options.DisableAnrIntegration();
 
-        Debug.Log("Sentry: BuildTimeOptions::Configure() finished");
+        // These options will get overwritten by CI. This allows us to create artifacts for both initialization types.
+        options.AndroidNativeInitializationType = NativeInitializationType.Runtime;
+        options.IosNativeInitializationType = NativeInitializationType.Runtime;
+
+        Debug.Log("Sentry: OptionsConfig::Configure() finished");
     }
 }
