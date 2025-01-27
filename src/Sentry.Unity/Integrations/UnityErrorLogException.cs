08f22e74aa76f4dcce8f0b97f8e28fd0acdac241
bitsandfoxes <reg@bitfox.at>
2025-01-27 11:26:53 +0100

    .

--- src/Sentry.Unity/Integrations/UnityErrorLogException.cs
+++ src/Sentry.Unity/Integrations/UnityErrorLogException.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Runtime.InteropServices.ComTypes;
 using Sentry.Extensibility;
 using Sentry.Protocol;
 using UnityEngine;
