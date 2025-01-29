003a4d27a0097b9b2106bec00213aff15aeeec51
Stefan Jandl <reg@bitfox.at>
2025-01-29 17:26:52 +0100

    feat: Separate CI jobs into workflows (#1989)

--- scripts/smoke-test-ios.ps1
+++ scripts/smoke-test-ios.ps1
@@ -21,7 +21,7 @@ Write-Host "Args received Action=$Action, SelectedRuntime=$SelectedRuntime, IsIn
 $ProjectName = "Unity-iPhone"
 $XcodeArtifactPath = "samples/IntegrationTest/Build"
 $ArchivePath = "$XcodeArtifactPath/archive"
-$AppPath = "$ArchivePath/$ProjectName/Build/Products/Release-iphonesimulator/IntegrationTest.app"
+$AppPath = "$XcodeArtifactPath/IntegrationTest.app"
 $AppName = "com.DefaultCompany.IntegrationTest"
 
 Class AppleDevice
