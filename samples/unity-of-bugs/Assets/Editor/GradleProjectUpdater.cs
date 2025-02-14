109c43cac64c6f14528a2e5c31594a03ee9c457d
bitsandfoxes <reg@bitfox.at>
2025-02-14 13:05:23 +0100

    added workaround to samples

---
+++ samples/unity-of-bugs/Assets/Editor/GradleProjectUpdater.cs
@@ -0,0 +1,17 @@
+﻿#if UNITY_ANDROID && !UNITY_2020_1_OR_NEWER
+using UnityEditor;
+using UnityEditor.Callbacks;
+using UnityEngine;
+
+namespace Editor
+{
+    public class GradleProjectUpdater : MonoBehaviour
+    {
+        [PostProcessBuild]
+        public static void OnPostProcessBuild(BuildTarget target, string pathToBuildProject)
+        {
+            PlayerSettings.Android.minSdkVersion = AndroidSdkVersions.AndroidApiLevel21;
+        }
+    }
+}
+#endif
