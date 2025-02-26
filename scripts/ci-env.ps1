429bdb0b67b78e620d6ba6a4725ff1f977c85a28
vaind <6349682+vaind@users.noreply.github.com>
2025-02-26 02:23:41 +0000

    chore: update to Unity 6000.0.40f1

--- scripts/ci-env.ps1
+++ scripts/ci-env.ps1
@@ -20,7 +20,7 @@ switch ($name) {
         return "2023.2.20f1"
     }
     "unity6000" {
-        return "6000.0.39f1"
+        return "6000.0.40f1"
     }
     Default {
         throw "Unkown variable '$name'"
