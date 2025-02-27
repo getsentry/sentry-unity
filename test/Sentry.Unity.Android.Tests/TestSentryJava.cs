43b1486308cf6ca778d3d54c1075af1939cd3d76
bitsandfoxes <reg@bitfox.at>
2025-02-27 11:22:18 +0100

    test

--- test/Sentry.Unity.Android.Tests/TestSentryJava.cs
+++ test/Sentry.Unity.Android.Tests/TestSentryJava.cs
@@ -19,7 +19,7 @@ internal class TestSentryJava : ISentryJava
     public bool? CrashedLastRun(IJniExecutor jniExecutor) => IsCrashedLastRun;
 
     public void Close(IJniExecutor jniExecutor) { }
-    public void ContinueTrace(IJniExecutor jniExecutor, string traceId) { }
+    public void SetTrace(IJniExecutor jniExecutor, string traceId, string spanId) { }
 
     public void WriteScope(
         IJniExecutor jniExecutor,
