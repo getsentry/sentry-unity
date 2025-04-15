1924cc94df4d3a5bcb30ab1d766461d65e7db268
bitsandfoxes <reg@bitfox.at>
2025-04-15 23:12:05 +0200

    reverted changes to sentryversion

--- src/Sentry.Unity.Editor/SentryUnityVersion.cs
+++ src/Sentry.Unity.Editor/SentryUnityVersion.cs
@@ -2,29 +2,19 @@ using System;
 using System.Text.RegularExpressions;
 using Sentry.Unity.Integrations;
 
-namespace Sentry.Unity;
+namespace Sentry.Unity.Editor;
 
 internal static class SentryUnityVersion
 {
-    // Internal for tests
-    internal static Version? Version;
-
     public static bool IsNewerOrEqualThan(string version, IApplication? application = null)
         => GetVersion(application) >= new Version(version);
 
-    internal static Version GetVersion(IApplication? application = null)
+    internal static Version? GetVersion(IApplication? application = null)
     {
-        if (Version is not null)
-        {
-            return Version;
-        }
-
         application ??= ApplicationAdapter.Instance;
-
         // The Unity version format looks like this: '2019.4.38f1', '2022.1.0a17' or '2022.1.1b4',
         // but Version() expects only the numerical parts, e.g. `2021.1.0`
         var unityVersion = Regex.Replace(application.UnityVersion, "^([0-9]+\\.[0-9]+\\.[0-9]+)[a-z].*$", "$1");
-        Version = new Version(unityVersion);
-        return Version;
+        return new Version(unityVersion);
     }
 }
