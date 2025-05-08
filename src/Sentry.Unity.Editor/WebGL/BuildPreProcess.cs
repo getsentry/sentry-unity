2680c634ae37d8782982dc347d01b6fa77468da4
bitsandfoxes <reg@bitfox.at>
2025-05-08 11:26:42 +0200

    added log for limitly supported settings

--- src/Sentry.Unity.Editor/WebGL/BuildPreProcess.cs
+++ src/Sentry.Unity.Editor/WebGL/BuildPreProcess.cs
@@ -1,6 +1,8 @@
+using Sentry.Extensibility;
 using UnityEditor;
 using UnityEditor.Build;
 using UnityEditor.Build.Reporting;
+using UnityEngine;
 
 namespace Sentry.Unity.Editor.WebGL;
 
@@ -21,6 +23,15 @@ internal class BuildPreProcess : IPreprocessBuildWithReport
             throw new BuildFailedException("WebGL exception support is set to None. The Sentry SDK requires exception " +
                 "support to function properly. Please change the WebGL exception support setting in Player Settings " +
                 "or disable the Sentry SDK.");
+        }
+        else if (PlayerSettings.WebGL.exceptionSupport != WebGLExceptionSupport.FullWithStacktrace)
+        {
+            var (options, _) = SentryScriptableObject.ConfiguredBuildTimeOptions();
+            var logger = options?.DiagnosticLogger ?? new UnityLogger(options ?? new SentryUnityOptions());
+
+            logger.LogWarning("The SDK requires the Exception Support to be set " +
+                              "'WebGLExceptionSupport.FullWithStacktrace' to be able to provide stack traces when " +
+                              "capturing errors.");
         }
     }
 }
