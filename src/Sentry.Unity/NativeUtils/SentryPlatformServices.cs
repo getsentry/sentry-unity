00e788aaf8026d25ea0a48c284cf92e6383d03a0
bitsandfoxes <reg@bitfox.at>
2025-07-07 16:39:35 +0200

    added summary to platformservices

--- src/Sentry.Unity/NativeUtils/SentryPlatformServices.cs
+++ src/Sentry.Unity/NativeUtils/SentryPlatformServices.cs
@@ -2,6 +2,12 @@ using System;
 
 namespace Sentry.Unity.NativeUtils;
 
+/// <summary>
+/// These are SDK's services that are only available at runtime and cannot be baked into the SDK. The
+/// <c>SentryInitialization.cs</c> is provided as <c>.cs</c> and gets compiled with the game. It sets <c>IUnityInfo</c>
+/// and the <c>PlatformConfiguration</c> callback during the game's startup so that they are available during initializtion.
+/// </summary>
+/// <remarks>Consider this <c>internal</c>.</remarks>
 public static class SentryPlatformServices
 {
     public static ISentryUnityInfo? UnityInfo { get; set; }
