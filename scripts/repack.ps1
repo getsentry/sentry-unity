f4a587803e2723cc1827a987fecc22691b50ea89
Stefan Jandl <reg@bitfox.at>
2025-08-05 14:48:28 +0200

    chore: Clean `package-release` before repacking (#2266)

--- scripts/repack.ps1
+++ scripts/repack.ps1
@@ -1,3 +1,12 @@
+# Clean up previous release artifacts
+if (Test-Path "package-release") {
+    Remove-Item -Path "package-release" -Recurse -Force
+}
+
+if (Test-Path "package-release.zip") {
+    Remove-Item -Path "package-release.zip" -Force
+}
+
 assemblyalias --target-directory "package-dev/Runtime" --internalize --prefix "Sentry." --assemblies-to-alias "Microsoft*;System*"
 assemblyalias --target-directory "package-dev/Editor" --internalize --prefix "Sentry." --assemblies-to-alias "Microsoft*;Mono.Cecil*"
 
