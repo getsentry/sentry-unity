000c924a84f97972e0c40d938f35b41d9d018390
bitsandfoxes <reg@bitfox.at>
2025-08-05 13:38:14 +0200

    There is no 2023 anymore

--- scripts/ci-env.ps1
+++ scripts/ci-env.ps1
@@ -13,9 +13,6 @@ switch ($name) {
     "unity2022.3" {
         return "2022.3.62f1"
     }
-    "unity2023" {
-        return "2023.2.20f1"
-    }
     "unity6000.0" {
         return "6000.0.54f1"
     }
