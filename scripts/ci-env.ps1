93921e9202a08c257e17bc9a6925e783cddde296
vaind <6349682+vaind@users.noreply.github.com>
2025-09-09 02:26:20 +0000

    chore: update to Unity 6000.0.57f1

--- scripts/ci-env.ps1
+++ scripts/ci-env.ps1
@@ -14,7 +14,7 @@ switch ($name) {
         return "2022.3.62f1"
     }
     "unity6000.0" {
-        return "6000.0.56f1"
+        return "6000.0.57f1"
     }
     "unity6000.1" {
         return "6000.1.16f1"
