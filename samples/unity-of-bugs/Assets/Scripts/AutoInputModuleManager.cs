7865c3195eb40649d8d28820de9d95b5e64fe7a1
bitsandfoxes <reg@bitfox.at>
2025-09-17 11:37:32 +0200

    Added fallback

--- samples/unity-of-bugs/Assets/Scripts/AutoInputModuleManager.cs
+++ samples/unity-of-bugs/Assets/Scripts/AutoInputModuleManager.cs
@@ -20,6 +20,8 @@ namespace Sentry.Unity.Samples
             gameObject.AddComponent<InputSystemUIInputModule>();
 #elif !ENABLE_INPUT_SYSTEM && ENABLE_LEGACY_INPUT_MANAGER
             gameObject.AddComponent<StandaloneInputModule>();
+#elif ENABLE_INPUT_SYSTEM && ENABLE_LEGACY_INPUT_MANAGER
+            gameObject.AddComponent<InputSystemUIInputModule>();
 #else
             Debug.LogError("Failed to detect input system. Sample scene might be unresponsive.");
 #endif
