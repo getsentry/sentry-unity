2fdca94c8c1e5800ad5a647ec6c9b10b2202e866
bitsandfoxes <reg@bitfox.at>
2025-12-12 09:57:08 +0100

    Updated spanID name for logs

--- src/Sentry.Unity/Integrations/UnityApplicationLoggingIntegration.cs
+++ src/Sentry.Unity/Integrations/UnityApplicationLoggingIntegration.cs
@@ -130,7 +130,7 @@ internal class UnityApplicationLoggingIntegration : ISdkIntegration
         _options.LogDebug("Capturing structured log message of type '{0}'.", logType);
 
         SentryLog.GetTraceIdAndSpanId(_hub, out var traceId, out var spanId);
-        SentryLog log = new(_clock.GetUtcNow(), traceId, ToLogLevel(logType), message) { ParentSpanId = spanId };
+        SentryLog log = new(_clock.GetUtcNow(), traceId, ToLogLevel(logType), message) { SpanId = spanId };
 
         log.SetDefaultAttributes(_options, UnitySdkInfo.Sdk);
         log.SetOrigin("auto.log.unity");
