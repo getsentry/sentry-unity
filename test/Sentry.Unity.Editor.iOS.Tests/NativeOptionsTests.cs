98434853ee13c23434a577e2e83d325627c68c37
bitsandfoxes <reg@bitfox.at>
2026-01-16 14:43:56 +0100

    Removed test

--- test/Sentry.Unity.Editor.iOS.Tests/NativeOptionsTests.cs
+++ test/Sentry.Unity.Editor.iOS.Tests/NativeOptionsTests.cs
@@ -28,28 +28,6 @@ public class NativeOptionsTests
     }
 
     [Test]
-    public void GenerateOptions_NewSentryOptionsGarbageAppended_FailsToCompile()
-    {
-        if (Application.platform != RuntimePlatform.OSXEditor)
-        {
-            Assert.Inconclusive("Skipping: Not on macOS");
-        }
-
-        const string testOptionsFileName = "testOptions.m";
-        var nativeOptionsString = NativeOptions.Generate(new SentryUnityOptions());
-        nativeOptionsString += "AppendedTextToFailCompilation";
-
-        File.WriteAllText(testOptionsFileName, nativeOptionsString);
-
-        var process = Process.Start("clang", $"-fsyntax-only -framework Foundation {testOptionsFileName}");
-        process.WaitForExit();
-
-        Assert.AreEqual(1, process.ExitCode);
-
-        File.Delete(testOptionsFileName);
-    }
-
-    [Test]
     public void CreateOptionsFile_NewSentryOptions_FileCreated()
     {
         const string testOptionsFileName = "testOptions.m";
