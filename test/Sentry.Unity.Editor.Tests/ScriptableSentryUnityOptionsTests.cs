7b9577075e0b892947766a67a589429d3a9cd599
bitsandfoxes <reg@bitfox.at>
2026-01-29 13:53:03 +0100

    Fixed test one more time

--- test/Sentry.Unity.Editor.Tests/ScriptableSentryUnityOptionsTests.cs
+++ test/Sentry.Unity.Editor.Tests/ScriptableSentryUnityOptionsTests.cs
@@ -21,8 +21,8 @@ public class ScriptableSentryUnityOptionsTests
         StringAssert.Contains("Enabled", optionsAsString);
         StringAssert.Contains("Dsn", optionsAsString);
         StringAssert.Contains("CaptureInEditor", optionsAsString);
-        StringAssert.Contains("EnableErrorEventThrottling", optionsAsString);
-        StringAssert.Contains("ErrorEventThrottleDedupeWindow", optionsAsString);
+        StringAssert.Contains("EnableThrottling", optionsAsString);
+        StringAssert.Contains("ThrottleDedupeWindow", optionsAsString);
         StringAssert.Contains("TracesSampleRate", optionsAsString);
         StringAssert.Contains("AutoSessionTracking", optionsAsString);
         StringAssert.Contains("AutoSessionTrackingInterval", optionsAsString);
