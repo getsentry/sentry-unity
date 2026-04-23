87f70934fcb82bedb6fb3e9c5540526ab51a1c94
vaind <6349682+vaind@users.noreply.github.com>
2026-04-23 02:57:23 +0000

    chore: update to Unity 6000.3.14f1

--- scripts/ci-env.ps1
+++ scripts/ci-env.ps1
@@ -14,7 +14,7 @@ switch ($name) {
         return "6000.0.72f1"
     }
     "unity6000.3" {
-        return "6000.3.12f1"
+        return "6000.3.14f1"
     }
     Default {
         throw "Unkown variable '$name'"
